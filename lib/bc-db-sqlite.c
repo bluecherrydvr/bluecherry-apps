dac6e880427b1e7be0d09caecd5631551102bbc8
Ben Collins <bcollins@bluecherry.net>
2010-04-20 15:51:01 -0400

    Update to sqlite3
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- lib/bc-db-sqlite.c
+++ lib/bc-db-sqlite.c
@@ -6,33 +6,47 @@
 
 #include <libbluecherry.h>
 
-#include <sqlite.h>
+#include <sqlite3.h>
 
 static void *bc_db_sqlite_open(struct config_t *cfg)
 {
+	sqlite3 *sqlite;
 	const char *file;
 
 	if (!config_lookup_string(cfg, BC_CONFIG_DB ".file", &file))
 		return NULL;
 
-	return sqlite_open(file, 0, NULL);
+	if (sqlite3_open(file, &sqlite)) {
+		sqlite3_close(sqlite);
+		return NULL;
+	}
+
+	return sqlite;
 }
 
 static void bc_db_sqlite_close(void *handle)
 {
-	sqlite_close(handle);
+	sqlite3_close(handle);
 }
 
 static int bc_db_sqlite_get_table(void *handle, int *nrows, int *ncols,
 				  char ***res, const char *fmt, va_list ap)
 {
-	return sqlite_get_table_vprintf(handle, fmt, res, nrows, ncols,
-					NULL, ap);
+	int ret;
+	char *query = sqlite3_vmprintf(fmt, ap);
+
+	if (query == NULL)
+		return -1;
+
+	ret = sqlite3_get_table(handle, query, res, nrows, ncols, NULL);
+	sqlite3_free(query);
+
+	return ret;
 }
 
 static void bc_db_sqlite_free_table(void *handle, char **res)
 {
-	sqlite_free_table(res);
+	sqlite3_free_table(res);
 }
 
 struct bc_db_ops bc_db_sqlite = {
