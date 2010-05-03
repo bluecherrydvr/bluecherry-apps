bd919ccde54f3faea3c8fc6620ae042671607e97
Ben Collins <bcollins@bluecherry.net>
2010-05-03 11:35:11 -0400

    sql: Add fields for audio and video params
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- misc/sql/v2-sqlite-dump.sql
+++ misc/sql/v2-sqlite-dump.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS AvailableSources;
 CREATE TABLE "AvailableSources" (
     id integer PRIMARY KEY NOT NULL,
     device character(255),
@@ -6,6 +7,7 @@ CREATE TABLE "AvailableSources" (
     path character(255)
 );
 
+DROP TABLE IF EXISTS Devices;
 CREATE TABLE "Devices" (
     id integer PRIMARY KEY NOT NULL,
     device_name character(100),
@@ -21,11 +23,16 @@ CREATE TABLE "Devices" (
     channel character(255),
     source_audio_in character(255),
     source_audio_out character(255),
+    audio_volume smallint,
+    audio_rate integer,
+    audio_format integer,
+    audio_channels smallint,
     saturation smallint,
     brightness smallint,
     hue smallint,
     contrast smallint,
     video_quality smallint DEFAULT 2,
+    video_interval smallint,
     signal_type character(6),
     buffer_prerecording smallint DEFAULT 5,
     buffer_postrecording smallint DEFAULT 5,
@@ -36,14 +43,28 @@ CREATE TABLE "Devices" (
     preset_type_ID smallint,
     motion_detection_on boolean,
     motion_detection_threshold integer,
-    file_chop_type smallint
+    file_chop_interval smallint
 );
 
-/* file_chop_type: 0 = None, 1 = Hourly, 2 = Daily
+/* file_chop_type: 0 = None, anything else is X hours
  * Only counts when motion_detection_on == false */
 
 -- video_quality: 1 = Low, 2 = Medium, 3 = High
 
+/* audio_format: 0x00ffffff are types, 0xff000000 are flags
+ * 0x00000001 U8
+ * 0x00000002 S8
+ * 0x00000004 U16_LE
+ * 0x00000008 S16_LE (default?)
+ * 0x00000010 U16_BE
+ * 0x00000020 S16_BE
+ * 
+ * 0x01000000 G723_24 - Type above is actually g723-24 data (u8?)
+ */
+
+-- video_interval: < 1 == disabled, otherwise framerate/interval
+
+DROP TABLE IF EXISTS Events;
 CREATE TABLE "Events" (
     id integer PRIMARY KEY NOT NULL,
     source_ID integer,
@@ -55,12 +76,14 @@ CREATE TABLE "Events" (
     archived boolean DEFAULT false
 );
 
+DROP TABLE IF EXISTS GlobalSettings;
 CREATE TABLE "GlobalSettings" (
     id integer PRIMARY KEY NOT NULL,
     parameter character(60),
     value character(255)
 );
 
+DROP TABLE IF EXISTS Users;
 CREATE TABLE "Users" (
     id integer PRIMARY KEY NOT NULL,
     login character(40),
