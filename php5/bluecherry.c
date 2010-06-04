1300535136eb04910d8fcd289d626fdd29525f2a
Ben Collins <bcollins@bluecherry.net>
2010-06-04 09:55:49 -0400

    php5: Add db functions
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- php5/bluecherry.c
+++ php5/bluecherry.c
@@ -11,37 +11,38 @@
 #include "libbluecherry.h"
 #include "php.h"
 
-typedef struct {
-	struct bc_handle *bch;
-} bch_ctx_t;
+#define BCH_NAME	"BC Handle"
+#define BCDB_NAME	"BC-DB Handle"
 
-typedef struct {
-	struct bc_db_handle *bcdb;
-} bcdb_ctx_t;
+static int bch_id;
+static int bcdb_id;
 
-static void bch_ctx_destructor(zend_rsrc_list_entry *rsrc TSRMLS_DC)
+static void bch_destructor(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 {
-	bch_ctx_t *ctx = (bch_ctx_t *)rsrc->ptr;
+	struct bc_handle *bch = (struct bc_handle *)rsrc->ptr;
 
-	if (ctx) {
-		if (ctx->bch) {
-			bc_handle_free(ctx->bch);
-			ctx->bch = NULL;
-		}
-		efree(ctx);
-	}
+	if (bch)
+		bc_handle_free(bch);
 
 	return;
 }
 
-static int bch_ctx_id;
-#define bch_ctx_name "BC Handle context"
+static void bcdb_destructor(zend_rsrc_list_entry *rsrc TSRMLS_DC)
+{
+	struct bc_db_handle *bcdb;
+
+	if (bcdb)
+		bc_db_close(bcdb);
+
+	return;
+}
 
 PHP_MINIT_FUNCTION(bluecherry)
 {
-	bch_ctx_id = zend_register_list_destructors_ex(bch_ctx_destructor,
-						       NULL, bch_ctx_name,
-						       module_number);
+	bch_id = zend_register_list_destructors_ex(bch_destructor, NULL,
+						   BCH_NAME, module_number);
+	bcdb_id = zend_register_list_destructors_ex(bcdb_destructor, NULL,
+						    BCDB_NAME, module_number);
 
 	return SUCCESS;
 }
@@ -61,21 +62,106 @@ PHP_MINFO_FUNCTION(bluecherry)
 }
 
 #define BCH_GET_RES(__func) do {					\
+	zval *z_ctx;							\
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r",	\
 				  &z_ctx) == FAILURE)			\
 		RETURN_FALSE;						\
-	ZEND_FETCH_RESOURCE(ctx, bch_ctx_t *, &z_ctx, -1, bch_ctx_name,	\
-			    bch_ctx_id);				\
-	if (ctx == NULL || ctx->bch == NULL) {				\
+	ZEND_FETCH_RESOURCE(bch, struct bc_handle *, &z_ctx, -1,	\
+			    BCH_NAME, bch_id);				\
+	if (bch == NULL) {						\
 		php_error_docref(NULL TSRMLS_CC, E_WARNING,		\
 			__func ": invalid context");			\
 		RETURN_FALSE;						\
 	}								\
-}while(0)
+} while(0)
+
+#define BCDB_GET_RES(__func) do {					\
+	zval *z_ctx;							\
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r",	\
+				  &z_ctx) == FAILURE)			\
+		RETURN_FALSE;						\
+	ZEND_FETCH_RESOURCE(bcdb, struct bc_db_handle *, &z_ctx, -1,	\
+			    BCDB_NAME, bcdb_id);			\
+	if (bcdb == NULL) {						\
+		php_error_docref(NULL TSRMLS_CC, E_WARNING,		\
+			__func ": invalid context");			\
+		RETURN_FALSE;						\
+	}								\
+} while(0)
+
+PHP_FUNCTION(bc_db_open)
+{
+	struct bc_db_handle *bcdb;
+
+	if ((bcdb = bc_db_open()) == NULL)
+		RETURN_FALSE;
+
+	ZEND_REGISTER_RESOURCE(return_value, bcdb, bcdb_id);
+}
+
+PHP_FUNCTION(bc_db_close)
+{
+	struct bc_db_handle *bcdb;
+
+	BCDB_GET_RES("bc_db_close");
+
+	bc_db_close(bcdb);
+
+	zend_list_delete((long)bcdb);
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(bc_db_get_table)
+{
+	struct bc_db_handle *bcdb;
+	zval *z_ctx;
+	int nrows, ncols;
+	char *sql;
+	int sql_len;
+	char **rows;
+	int i;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rs", &z_ctx,
+				  &sql, &sql_len) == FAILURE)
+		RETURN_FALSE;
+	ZEND_FETCH_RESOURCE(bcdb, struct bc_db_handle *, &z_ctx, -1,
+			    BCDB_NAME, bcdb_id);
+	if (bcdb == NULL) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING,
+				 "bc_db_get_table: invalid context");
+		RETURN_FALSE;
+	}
+
+	i = bc_db_get_table(bcdb, &nrows, &ncols, &rows, sql);
+	if (i || !nrows)
+		RETURN_FALSE;
+
+	array_init(return_value);
+
+	for (i = 0; i < nrows; i++) {
+		zval *row_arr;
+		int t;
+
+		ALLOC_INIT_ZVAL(row_arr);
+		array_init(row_arr);
+
+		for (t = 0; t < ncols; t++) {
+			char *str = rows[((i + 1) * ncols) + t];
+			if (str == NULL)
+				continue;
+			add_assoc_string(row_arr, rows[t], str, 1);
+		}
+
+		add_next_index_zval(return_value, row_arr);
+	}
+
+	bc_db_free_table(bcdb, rows);
+}
 
 PHP_FUNCTION(bc_handle_get)
 {
-	bch_ctx_t *ctx;
+	struct bc_handle *bch;
 	char *devname;
 	int devname_len;
 
@@ -83,40 +169,32 @@ PHP_FUNCTION(bc_handle_get)
 				  &devname_len) == FAILURE)
 		RETURN_FALSE;
 
-	if ((ctx = emalloc(sizeof(*ctx))) == NULL)
+	if ((bch = bc_handle_get(devname)) == NULL)
 		RETURN_FALSE;
 
-	if ((ctx->bch = bc_handle_get(devname)) == NULL) {
-		efree(ctx);
-		RETURN_FALSE;
-	}
-
-	ZEND_REGISTER_RESOURCE(return_value, ctx, bch_ctx_id);
+	ZEND_REGISTER_RESOURCE(return_value, bch, bch_id);
 }
 
 PHP_FUNCTION(bc_handle_free)
 {
-	zval *z_ctx;
-	bch_ctx_t *ctx;
+	struct bc_handle *bch;
 
 	BCH_GET_RES("bc_handle_free");
 
-	bc_handle_free(ctx->bch);
-	ctx->bch = NULL;
+	bc_handle_free(bch);
 
-	zend_list_delete(Z_LVAL_P(z_ctx));
+	zend_list_delete((long)bch);
 
 	RETURN_TRUE;
 }
 
 PHP_FUNCTION(bc_handle_start)
 {
-	zval *z_ctx;
-	bch_ctx_t *ctx;
+	struct bc_handle *bch;
 
 	BCH_GET_RES("bc_handle_start");
 
-	if (bc_handle_start(ctx->bch) < 0)
+	if (bc_handle_start(bch) < 0)
 		RETURN_FALSE;
 
 	RETURN_TRUE;
@@ -124,51 +202,52 @@ PHP_FUNCTION(bc_handle_start)
 
 PHP_FUNCTION(bc_buf_get)
 {
-	zval *z_ctx;
-	bch_ctx_t *ctx;
+	struct bc_handle *bch;
 
 	BCH_GET_RES("bc_buf_get");
 
-	bc_buf_get(ctx->bch);
+	bc_buf_get(bch);
 
 	RETURN_TRUE;
 }
 
 PHP_FUNCTION(bc_buf_size)
 {
-	zval *z_ctx;
-	bch_ctx_t *ctx;
+	struct bc_handle *bch;
 
 	BCH_GET_RES("bc_buf_size");
 
-	RETURN_LONG(bc_buf_size(ctx->bch));
+	RETURN_LONG(bc_buf_size(bch));
 }
 
 PHP_FUNCTION(bc_buf_data)
 {
-	zval *z_ctx;
-	bch_ctx_t *ctx;
+	struct bc_handle *bch;
         
 	BCH_GET_RES("bc_buf_data");
 
-	RETURN_STRINGL(bc_buf_data(ctx->bch), bc_buf_size(ctx->bch), 1);
+	RETURN_STRINGL(bc_buf_data(bch), bc_buf_size(bch), 1);
 }
 
 PHP_FUNCTION(bc_set_mjpeg)
 {
-	zval *z_ctx;
-	bch_ctx_t *ctx;
+	struct bc_handle *bch;
 
 	BCH_GET_RES("bc_set_mjpeg");
 
-	ctx->bch->vfmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
-	if (ioctl(ctx->bch->dev_fd, VIDIOC_S_FMT, &ctx->bch->vfmt) < 0)
+	bch->vfmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+	if (ioctl(bch->dev_fd, VIDIOC_S_FMT, &bch->vfmt) < 0)
 		RETURN_FALSE;
 
 	RETURN_TRUE;
 }
 
 static function_entry bluecherry_functions[] = {
+	/* Bluecherry DB Handlers */
+	PHP_FE(bc_db_open, NULL)
+	PHP_FE(bc_db_close, NULL)
+	PHP_FE(bc_db_get_table, NULL)
+	/* Bluecherry Video Handlers */
 	PHP_FE(bc_handle_get, NULL)
 	PHP_FE(bc_handle_free, NULL)
 	PHP_FE(bc_handle_start, NULL)
