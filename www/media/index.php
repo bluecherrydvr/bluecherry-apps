c286ca4640a246b24022933eac3f6e93c40e9913
Ben Collins <bcollins@bluecherry.net>
2010-10-11 13:12:55 -0400

    www/media: Add export of media table via XML
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

---
+++ www/media/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$db = bc_db_open() or die("Could not open database\n");
+
+$events = bc_db_get_table($db, "SELECT * FROM Media ORDER BY time DESC");
+
+# Output header for this feed
+print "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+
+# Output one item for each event
+foreach ($events as $item) {
+	print "  <item>\n";
+
+	foreach (array_keys($item) as $key)
+		print "    <$key>". $item[$key] ."</$key>\n";
+
+	print "  </item>\n";
+}
+
+bc_db_close($db);
+?>
