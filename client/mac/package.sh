6e9cea1aa7d720045f90f9905eeba531d1a70d78
John Brooks <john.brooks@bluecherry.net>
2010-11-09 12:22:17 -0700

    client: Use 'Bluecherry Client' as the final bundle name on OS X

--- client/mac/package.sh
+++ client/mac/package.sh
@@ -10,5 +10,10 @@ if [ ! -e $1/BluecherryClient.app ]; then
 	exit
 fi
 
-hdiutil create -srcfolder $1/BluecherryClient.app BluecherryClient.dmg
+rm -r "$1/Bluecherry Client.app"
+cp -R "$1/BluecherryClient.app" "$1/Bluecherry Client.app"
+
+hdiutil create -srcfolder "$1/Bluecherry Client.app" BluecherryClient.dmg
 hdiutil internet-enable -yes BluecherryClient.dmg
+
+rm -r "$1/Bluecherry Client.app"
