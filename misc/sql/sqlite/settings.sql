cf2703bddada482e5b2660399b44e00ad2d2a5ca
Ben Collins <bcollins@bluecherry.net>
2010-11-10 15:21:42 -0500

    sql: Add default name and email for global settings
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- misc/sql/sqlite/settings.sql
+++ misc/sql/sqlite/settings.sql
@@ -2,3 +2,6 @@ CREATE TABLE GlobalSettings (
 	parameter varchar(60) PRIMARY KEY NOT NULL,
 	value varchar(255)
 );
+
+INSERT INTO 'GlobalSettings' VALUES ('1', 'G_DVR_NAME', 'Bluecherry DVR v2');
+INSERT INTO 'GlobalSettings' VALUES ('2', 'G_DVR_EMAIL', 'root@localhost');
