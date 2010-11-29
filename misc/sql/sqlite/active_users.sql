5b9883d162cb02e12ddd69054e02ac5f1af76bd9
Alex Belykh <abelykh@corp.bluecherry.net>
2010-11-29 14:46:20 -0600

    --ActiveUsers table sql

---
+++ misc/sql/sqlite/active_users.sql
@@ -0,0 +1,7 @@
+
+#
+# Table structure for table: ActiveUsers
+#
+CREATE TABLE 'ActiveUsers' ( 'id' INT, 'ip' TEXT(20), 'from_client' BOOLEAN , 'time' INT, 'kick' BOOLEAN );
+# --------------------------------------------------------
+
