6d96106365859349ec0f033ba53933709ff651d9
Ben Collins <bcollins@bluecherry.net>
2010-12-16 14:49:12 -0500

    Fix sql files to be compliant (comment delimiters)
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- misc/sql/sqlite/active_users.sql
+++ misc/sql/sqlite/active_users.sql
@@ -1,7 +1,10 @@
-
-#
-# Table structure for table: ActiveUsers
-#
-CREATE TABLE 'ActiveUsers' ( 'id' INT, 'ip' TEXT(20), 'from_client' BOOLEAN , 'time' INT, 'kick' BOOLEAN );
-# --------------------------------------------------------
-
+--
+-- Table structure for table: ActiveUsers
+--
+CREATE TABLE 'ActiveUsers' (
+	'id' INT,
+	'ip' TEXT(20),
+	'from_client' BOOLEAN ,
+	'time' INT,
+	'kick' BOOLEAN
+);
