a0fea6a18b6d53fc7d6f6916ab2d2c3fb96d7923
Ben Collins <bcollins@bluecherry.net>
2011-01-14 11:52:52 -0500

    Add comments to Devices table
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- misc/sql/mysql/2-devices.sql
+++ misc/sql/mysql/2-devices.sql
@@ -10,17 +10,23 @@ CREATE TABLE AvailableSources (
 
 CREATE TABLE Devices (
 	id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,	-- Unique ID for this device
+
+	-- These are required
 	device_name varchar(100),		-- User given name
+	protocol varchar(50),			-- IP or V4L2
+
+	-- When protocol='V4L2' then same as AvailableSources.device
+	-- When protocol='IP' then URL to the device, e.g
+	--    rtsp://foo@bar:192.168.1.100:436/media.amp
+	device varchar(256),
+
+	-- When protocol='V4L2', then the same as AvailableSources.driver
+	-- When protocol='IP', then type of IP device (e.g. RTSP-AXIS, RTSP-GENERIC)
+	driver varchar(256),
+
+	-- The rest of these may or may not be supported by the device
 	resolutionX smallint,
 	resolutionY smallint,
-	protocol varchar(50),			-- IP or V4L2
-	-- V4L2 specific fields
-	device varchar(256),			-- Same as AvailableSources.device
-	-- IP specific fields
-	host_port varchar(256),			-- E.g. 192.168.1.100:436
-	-- Generic items (may or may ot be supported though )
-	driver varchar(256),			-- For IP, protocol, for V4L2,
-						-- the same as AvailableSources.driver
 	channel integer NOT NULL DEFAULT 0,	-- If device supports multiple channels
 	invert boolean NOT NULL DEFAULT 0,	-- True if cam is inverted
 	audio_volume smallint DEFAULT 50,	-- Audio gain (0 - 100)
