1e6a8fcdcb60843af9d096e99128686ce29c175f
Ben Collins <bcollins@bluecherry.net>
2011-01-26 00:43:39 -0500

    www: Change div class for ip cams to localDevice until ipDevice is defined a bit better
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- www/template/ajax/devices.php
+++ www/template/ajax/devices.php
@@ -8,7 +8,7 @@ echo "<div id='header'>".TOTAL_DEVICES."&nbsp;<b>{$devices->total_devices}</b></
 if ($devices->ip_cameras) {
 	echo "<div id='dvrCard'><div class='cardHeader'>IP Cameras</div><div class='cardContent'>";
 	foreach($devices->ip_cameras as $key => $device){
-		echo "<div id='ipDevice' class='{$device['id']}'><div>";
+		echo "<div id='localDevice' class='{$device['id']}'><div>";
 		echo "<div id='{$device['id']}' class='name'>";
 		echo "{$device['device_name']}</div></div>";
 		echo "<div id='status' id='{$device['id']}' class='{$device['status']}'>".constant('DEVICE_VIDEO_STATUS_'.$device['status'])." <a href='#' class='change_state' id='{$device['id']}'>[".constant('DEVICE_VIDEO_STATUS_CHANGE_'.$device['status'])."]</a></div>";
