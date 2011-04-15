509500db2edf09d47b0a20e0a2a3434987933431
Ben Collins <bcollins@bluecherry.net>
2011-04-15 14:18:27 -0400

    login.php: Check for from_client being defined
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- www/ajax/login.php
+++ www/ajax/login.php
@@ -3,7 +3,8 @@
 include("../lib/lib.php");  #common functions
 
 $user_to_login = new user('username', $_POST['login']);
-$result = $user_to_login->doLogin($_POST['password'], $_POST['from_client']);
+$result = $user_to_login->doLogin($_POST['password'],
+		defined($_POST['from_client']) ? $_POST['from_client'] : NULL);
 
 echo $result;
 
