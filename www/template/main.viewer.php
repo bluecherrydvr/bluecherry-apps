7354f87170072ba439ebd3a9ded7f2200dbc42f0
Ben Collins <bcollins@bluecherry.net>
2011-04-15 14:22:58 -0400

    main.viewer.php: Use new db class
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- www/template/main.viewer.php
+++ www/template/main.viewer.php
@@ -4,8 +4,8 @@
 
 #temp PTZ fix
 
-$db = DVRDatabase::getInstance();
-$ptzdevices = $db->DBFetchAll("SELECT * FROM Devices WHERE ptz_serial_values <> ''");
+$db = database::getInstance();
+$ptzdevices = $db->fetchAll("SELECT * FROM Devices WHERE ptz_serial_values <> ''");
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
