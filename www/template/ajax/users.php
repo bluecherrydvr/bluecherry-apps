49c1ab90bdd7a6af30f423a4abca0324b5098316
Alex Belykh <abelykh@corp.bluecherry.net>
2011-04-18 16:37:10 -0600

    -remove access list button from new user form

--- www/template/ajax/users.php
+++ www/template/ajax/users.php
@@ -22,7 +22,7 @@
 					".((!empty($dvr_users->data['new'])) ? "" : "<div id='deleteButton'>".DELETE_USER."</div>")."
 					<div id='saveButton'>".SAVE_CHANGES."</div>
 					<div>
-						<div id='editAccessList'>Edit access list</div>
+						".((!empty($dvr_users->data['new'])) ? "" : "<div id='editAccessList'>Edit access list</div>")."
 					</div>
 					<div><label>".USERS_NAME."<span class='sub'>".USERS_NAME_EX."</span></label><input type='text' name='name' value='{$dvr_users->data[0]['name']}'  /></div><br />
 					<div><label>".USERS_LOGIN."<span class='sub'>".USERS_LOGIN_EX."</span></label><input type='text' name='username' value='".((!empty($dvr_users->data['new'])) ? '' : $dvr_users->data[0]['username'])."' /></div><br />
