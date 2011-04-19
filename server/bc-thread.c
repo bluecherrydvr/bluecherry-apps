6e08a956ad0c284678f5f3609f15f21d3c22c300
Ben Collins <bcollins@bluecherry.net>
2011-04-19 18:38:29 -0400

    server: Fix reset of media files to avoid missing 1 second
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- server/bc-thread.c
+++ server/bc-thread.c
@@ -56,12 +56,14 @@ static int process_schedule(struct bc_record *bc_rec)
 	if (bc_rec->reset_vid ||
 	    (bc_media_length(&bc_rec->media) > BC_MAX_RECORD_TIME &&
 	    !bc_rec->sched_last)) {
+		if (bc_rec->reset_vid) {
+			bc_dev_info(bc_rec, "Resetting media due to reset_vid");
+			bc_rec->reset_vid = 0;
+			ret = 1;
+		}
 		bc_close_avcodec(bc_rec);
 		bc_handle_stop(bc);
-		bc_rec->reset_vid = 0;
 		try_formats(bc_rec);
-		if (bc_rec->reset_vid)
-			ret = 1;
 		bc_dev_info(bc_rec, "Reset media file");
 	} else if (bc_rec->sched_last) {
 		bc_close_avcodec(bc_rec);
