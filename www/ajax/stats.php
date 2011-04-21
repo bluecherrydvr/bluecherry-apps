a3f0e2e338141685902c1d9bdb795fcb793a1e8b
Ben Collins <bcollins@bluecherry.net>
2011-04-20 22:14:46 -0400

    stats.php: Rework cpu usage and mem usage
    Signed-off-by: Ben Collins <bcollins@bluecherry.net>

--- www/ajax/stats.php
+++ www/ajax/stats.php
@@ -13,77 +13,77 @@ session_write_close();
 #server stats: CPU/memusage
 
 class ServerStats{
-	public function __construct(){
+	public function __construct() {
 		$this->makeXML();
 	}
-		function getLoadAvg(){
-			/* DISABLED UNTIL MOVED TO SERVER 
-			for ($i=0; $i<=1; $i++){
-				$file = fopen("/proc/stat", "r");
-				$tmp = fgets($file, 4096);				
-				$tmp = preg_split("/[\s]+/", $tmp);
-
-				for ($j=1; $j<=4; $j++){
-					$reading[$i][$j-1]=$tmp[$j];	
-				};
-				sleep(1);
-			}
-			$tmp = ($reading[1][0]-$reading[0][0]+$reading[1][1]-$reading[0][1]+$reading[1][2]-$reading[0][2]);
-			return round($tmp/($tmp+$reading[1][3]-$reading[0][3])*100, 2);
-			*/ 
+	function getCPUUsage() {
+		$p = @popen("/usr/bin/mpstat", "r");
+		if (!$p)
 			return 0;
+		while ($str = fgets($p)) {
+			if (!strstr($str, "all"))
+				continue;
+			$cpu = preg_split("/[\s,]+/", $str);
+			break;
 		}
-		function getMemUse(){
-			$file = fopen("/proc/meminfo", "r");
-			for ($i=1; $i<=4; $i++){
-				$tmp = fgets($file, 4096); $tmp = preg_replace("/[^(0-9)]/", "", $tmp);
-				$memstat[$i] = $tmp;
-			};
-			$memstat[2] = $memstat[1] - ($memstat[2]+$memstat[3]+$memstat[4]);
-			$memstat[3] = round($memstat[2]/$memstat[1]*100, 2);
-			return $memstat;
+		pclose($p);
+		$id = count($cpu) - 2;
+		return round(100 - $cpu[$id], 2);
+	}
+	function getMemUse() {
+		$f = @fopen("/proc/meminfo", "r");
+		for ($i = 0; $str = fgets($f); $i++) {
+			$stat[$i] = intval(preg_replace("/.*\s(\d+)\s.*/", "$1",
+					   $str));
 		}
-		function getUpTime(){
-			$file = file_get_contents("/proc/uptime", "r");
-			$tmp = explode(" ", $file);
-			$dd = (round($tmp[0])<86400 || round($tmp[0])>= 172800) ? " \d\a\y\s " : " \d\a\y ";
-			//return date("j ".trim($dd)."\, H:i:s",-64800 + round($tmp[0]));
-			$d = intval($tmp[0]/86400); $tmp[0] -= $d*86400;
-			$h = str_pad(intval($tmp[0]/3600), 2, '0', STR_PAD_LEFT); $tmp[0] -= $h*3600;
- 			$m = str_pad(intval($tmp[0]/60), 2, '0', STR_PAD_LEFT); $tmp[0] -= $m*60;
-  			return $d.' day(s), '.$h.':'.$m.':'.str_pad(intval($tmp[0]), 2, '0', STR_PAD_LEFT);
-		}
-		private function isServerRunning(){
-			return (shell_exec('pidof bc-server')) ? true : false;
-		}
-		private function writeFailCheck(){
-			$time = file_exists(DB_WRITABLE) ? filemtime(DB_WRITABLE) : 0;
-			if ($time>43200){
-				return date("F d Y H:i:s.", $time);
-			} else {
-				return 'false';
-			};
-		}
-		function makeXML(){
-			$loadavg = $this->getLoadAvg();
-			$memstat = $this->getMemUse();
-			$uptime  = $this->getUpTime();
-			$server  = $this->isServerRunning();
-			$writefail = $this->writeFailCheck();
-			//simple XML output
-			header('Content-type: text/xml');
-			echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
-				  	<server-status>
-						<cpu-usage>$loadavg</cpu-usage>
-						<memory-total>".$memstat['1']."</memory-total>
-						<memory-inuse>".$memstat['2']."</memory-inuse>
-						<memory-used-percentage>".$memstat['3']."</memory-used-percentage>
-						<bc-server-running>".(($server)? 'up' : 'down')."</bc-server-running>
-						<server-uptime>$uptime</server-uptime>
-						<failed-write>$writefail</failed-write>
-				  	</server-status>
-			";
-		}
+		fclose($f);
+
+		$total = $stat[0];
+		$used = $total - ($stat[1] + $stat[2] + $stat[3]);
+		$percent = round($used / $total * 100, 2);
+
+		return array($total, $used, $percent);
+	}
+	function getUpTime(){
+		$file = file_get_contents("/proc/uptime", "r");
+		$tmp = explode(" ", $file);
+		$dd = (round($tmp[0])<86400 || round($tmp[0])>= 172800) ? " \d\a\y\s " : " \d\a\y ";
+		//return date("j ".trim($dd)."\, H:i:s",-64800 + round($tmp[0]));
+		$d = intval($tmp[0]/86400); $tmp[0] -= $d*86400;
+		$h = str_pad(intval($tmp[0]/3600), 2, '0', STR_PAD_LEFT); $tmp[0] -= $h*3600;
+		$m = str_pad(intval($tmp[0]/60), 2, '0', STR_PAD_LEFT); $tmp[0] -= $m*60;
+		return $d.' day(s), '.$h.':'.$m.':'.str_pad(intval($tmp[0]), 2, '0', STR_PAD_LEFT);
+	}
+	private function isServerRunning(){
+		return (shell_exec('pidof bc-server')) ? true : false;
+	}
+	private function writeFailCheck(){
+		$time = file_exists(DB_WRITABLE) ? filemtime(DB_WRITABLE) : 0;
+		if ($time>43200){
+			return date("F d Y H:i:s.", $time);
+		} else {
+			return 'false';
+		};
+	}
+	function makeXML(){
+		$cpu = $this->getCPUUsage();
+		$mem = $this->getMemUse();
+		$uptime  = $this->getUpTime();
+		$server  = $this->isServerRunning();
+		$writefail = $this->writeFailCheck();
+
+		header('Content-type: text/xml');
+		echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
+<server-status>
+	<cpu-usage>$cpu</cpu-usage>
+	<memory-total>{$mem[0]}</memory-total>
+	<memory-inuse>{$mem[1]}</memory-inuse>
+	<memory-used-percentage>{$mem[2]}</memory-used-percentage>
+	<bc-server-running>".(($server)? 'up' : 'down')."</bc-server-running>
+	<server-uptime>$uptime</server-uptime>
+	<failed-write>$writefail</failed-write>
+</server-status>\n";
+	}
 }
 
 $stats = new ServerStats;
