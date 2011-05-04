648fe6b3abba91ffcefa3f6d347261682a4056ab
Alex Belykh <abelykh@corp.bluecherry.net>
2011-05-04 15:41:50 -0600

    --web: fix ptzsettings -- do not construct camera/template if POST is set

--- www/ajax/ptzsettings.php
+++ www/ajax/ptzsettings.php
@@ -7,11 +7,9 @@ $current_user->checkAccessPermissions('admin');
 
 if (!empty($_POST)){
 	camera::updatePtzSettings($_POST['path'], $_POST['baud'], $_POST['bit'], $_POST['parity'], $_POST['stop_bit'], $_POST['protocol'], $_POST['addr'], $_POST['id']);
+} else {
+	$this_camera = new camera($_GET['id']);
+	include_once('../template/ajax/ptzsettings.php');
 };
-
-$this_camera = new camera($_GET['id']);
-
-include_once('../template/ajax/ptzsettings.php');
-
 
 ?>
