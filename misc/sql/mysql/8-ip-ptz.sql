43d4fd83911dfcedc9e500701efaefc4835ad013
Curtis Hall <curt@bluecherry.net>
2011-05-31 22:02:10 -0500

    Added ipCameraPTZDriver table

---
+++ misc/sql/mysql/8-ip-ptz.sql
@@ -0,0 +1,21 @@
+kCREATE TABLE `ipCameraPTZDriver` (
+  `id` varchar(32) NOT NULL,
+  `move_up` varchar(256) DEFAULT NULL,
+  `move_down` varchar(256) DEFAULT NULL,
+  `move_right` varchar(256) DEFAULT NULL,
+  `move_keft` varchar(256) DEFAULT NULL,
+  `move_upleft` varchar(256) DEFAULT NULL,
+  `move_upright` varchar(256) DEFAULT NULL,
+  `move_downleft` varchar(256) DEFAULT NULL,
+  `move_downright` varchar(256) DEFAULT NULL,
+  `zoom_in` varchar(256) DEFAULT NULL,
+  `zoom_out` varchar(256) DEFAULT NULL,
+  `focus_in` varchar(256) DEFAULT NULL,
+  `focus_out` varchar(256) DEFAULT NULL,
+  `preset_save` varchar(256) DEFAULT NULL,
+  `preset_call` varchar(256) DEFAULT NULL,
+  `preset_update` varchar(256) DEFAULT NULL,
+  `preset_delete` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
