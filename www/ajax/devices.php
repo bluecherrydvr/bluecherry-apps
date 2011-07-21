61fef676ebb0ceb240ff1a974563592568d2be2f
John Brooks <john.brooks@bluecherry.net>
2011-07-21 17:32:38 -0600

    Don't attempt to print array values in devices XML; it causes an error with htmlspecialchars

--- www/ajax/devices.php
+++ www/ajax/devices.php
@@ -67,7 +67,7 @@ class devices{
 				    !in_array($prop, $short_props))
 					continue;
 
-				if (in_array($prop, $block_props))
+				if (in_array($prop, $block_props) || is_array($val))
 					continue;
 
 				$xml .= "    <$prop>".htmlspecialchars($val)."</$prop>\n";
