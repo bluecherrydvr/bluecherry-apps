bluecherrydvr/bluecherry-apps

commit a60ad74db442d15b79d626069f96a3abe995113d

Bug #1021: Add room for null termination on the per-device schedule
Previously, the variable was exactly as large as its data; strlcpy
then did the right thing and truncated to set the last field to 0.
This resulted in the schedule defaulting to continuous at the last
hour of the schedule, which is 11pm saturday.

Fixed by null terminating both schedules (as I feel it's cleaner),
and making sure they have enough space.

server/bc-server.c
@@ -18,7 +18,7 @@ static int max_threads;
 static int cur_threads;
 static int record_id = -1;
 
-char global_sched[7 * 24];
+char global_sched[7 * 24 + 1];
 
 static pthread_mutex_t media_lock = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
 
@@ -84,6 +84,7 @@ static void bc_check_globals(void)
 	} else {
 		/* Default to continuous record */
 		memset(global_sched, 'C', sizeof(global_sched));
+		global_sched[sizeof(global_sched)-1] = 0;
 	}
 	bc_db_free_table(dbres);
 

lib/libbluecherry.h
@@ -128,7 +128,7 @@ struct bc_device_config {
 	char rtsp_password[64];
 	char signal_type[16];
 	char motion_map[769];
-	char schedule[7 * 24];
+	char schedule[7 * 24 + 1];
 	int     width, height;
 	int     interval;
 	int8_t  debug_level;

server/bc-server.h
@@ -102,7 +102,7 @@ struct bc_output_packet {
 /* Flags for aud_format */
 #define AUD_FMT_FLAG_G723_24	0x01000000
 
-extern char global_sched[7 * 24];
+extern char global_sched[7 * 24 + 1];
 
 void bc_get_media_loc(char *stor);
 

commit 05cc0d680ae9e57bbc7cfb438acdcdb988310648

Make global schedules work again..
Regression from a60ad74db4. At least I found it within 24 hours, and
before a release.

server/bc-server.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <sys/statvfs.h>
+#include <bsd/string.h>
 
 #include <libavutil/log.h>
 
@@ -79,8 +80,8 @@ static void bc_check_globals(void)
 
 	if (dbres != NULL && !bc_db_fetch_row(dbres)) {
 		const char *sched = bc_db_get_val(dbres, "value", NULL);
-		if (sched && strlen(sched) == sizeof(global_sched))
-			memcpy(global_sched, sched, sizeof(global_sched));
+		if (sched)
+			strlcpy(global_sched, sched, sizeof(global_sched));
 	} else {
 		/* Default to continuous record */
 		memset(global_sched, 'C', sizeof(global_sched));