00c593784a0b56fc41c4ed50591975dbc8b76341
John Brooks <john.brooks@bluecherry.net>
2012-09-25 17:18:55 -0600

    Convert bc-av functions into the media_writer class
    media_writer is responsible for muxing stream_packets conforming to
    one set of stream_property into a single file recording.
    
    This replaces bc_output_packet_write and bc_{open,close}_avcodec.
    
    WIP start converting bc-av into media_writer
    
    Rename bc-av

---
+++ server/media_writer.h
@@ -0,0 +1,29 @@
+#ifndef MEDIA_WRITER_H
+#define MEDIA_WRITER_H
+
+#include <string>
+
+#include "libbluecherry.h"
+
+struct AVFormatContext;
+struct AVStream;
+
+class media_writer
+{
+public:
+	media_writer();
+
+	int open(const std::string &file_path);
+	void close();
+
+	bool write_packet(const stream_packet &pkt);
+
+protected:
+	std::string file_path;
+	AVFormatContext *oc;
+	AVStream *video_st, *audio_st;
+	int64_t output_pts_base;
+};
+
+#endif
+
