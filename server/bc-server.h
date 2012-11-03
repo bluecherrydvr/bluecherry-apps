4438cb5bb1067f31434c764d1fbad8b5cd3d63bb
John Brooks <john.brooks@bluecherry.net>
2012-11-02 21:29:37 -0600

    Fix error events to only end when we've successfully read a packet

--- server/bc-server.h
+++ server/bc-server.h
@@ -18,8 +18,6 @@ extern "C" {
 /* Maximum length of recording */
 #define BC_MAX_RECORD_TIME 900
 
-// XXX Needs refactor with proper construction, due to non-POD types
-// XXX This will leak badly on destruction, due to prerecord_buffer
 class bc_record
 {
 public:
