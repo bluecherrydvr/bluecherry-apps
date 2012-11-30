0c063593fbf555d9e678a1fdfa2d7be416de427d
John Brooks <john.brooks@bluecherry.net>
2012-11-29 23:21:55 -0700

    Expand comments in input_device.h
    Signed-off-by: Ismael Luceno <ismael.luceno@corp.bluecherry.net>

--- lib/input_device.h
+++ lib/input_device.h
@@ -22,6 +22,10 @@
 class stream_packet;
 class stream_properties;
 
+/* input_device provides an abstract interface implemented by device backends, such
+ * as v4l2_device and rtp_device. Generally, each bc_handle has an input_device, which
+ * while running will be repeatedly polled with read_packet(), which should block until
+ * a packet is available. */
 class input_device
 {
 public:
@@ -34,6 +38,12 @@ public:
 
 	const char *get_error_message() const { return _error_message.c_str(); }
 
+	/* Read one packet from the device, blocking if necessary. Returns zero on
+	 * success, non-zero (possibly errno) on failure. On success, packet() will
+	 * retrieve the current packet until the next call to read_packet.
+	 *
+	 * The device must have successfully returned from start() prior to calling
+	 * read_packet(). */
 	virtual int read_packet() = 0;
 	virtual const stream_packet &packet() const = 0;
 
@@ -43,12 +53,19 @@ public:
 
 	/* Get the current stream properties; this is a temporary hack for streaming
 	 * and should not be relied on. Properties are included as a field with every
-	 * packet, and can be reliably updated that way. */
+	 * packet, and can be reliably updated that way.
+	 *
+	 * This should be removed once RTSP streaming is converted to a proper
+	 * stream_consumer. */
 	std::shared_ptr<const stream_properties> properties() const { return current_properties; }
 
 protected:
 	bool _audio_enabled, _started;
 	std::string _error_message;
+	/* Incremental counter for packets emitted by this device, which should increment
+	 * by exactly one for each successful return from read_packet(), beginning with
+	 * zero. Subclass is expected to handle this behavior in the packet creation logic.
+	 */
 	unsigned next_packet_seq;
 	std::shared_ptr<stream_properties> current_properties;
 
@@ -59,10 +76,14 @@ protected:
  * to AVCodecContext for the video and audio (if applicable) streams, and is used
  * for recording and decoding.
  *
- * An instance of this class is associated with each stream_packet, which will
- * usually be shared for all packets from the same input. The contents are immutable.
- * If the input properties change, a new instance will be created and sent with all
- * packets from where it began to apply.
+ * An instance of this class is associated with each stream_packet, which is usually
+ * shared and reference counted for packets from that input_device. The contents are
+ * immutable. If input properties change, a new instance of stream_properties is created
+ * and sent along with subsequent packets; users are expected to compare to determine
+ * when this happens.
+ *
+ * This behavior is necessary to ensure that property changes are always processed
+ * at the correct time, regardless of packet drops or buffering.
  */
 class stream_properties
 {
@@ -79,6 +100,8 @@ public:
 		std::vector<char> extradata;
 
 		video_properties();
+		/* Apply these properties to an allocated AVCodecContext instance,
+		 * suitable for creating encoders, decoders, etc. */
 		void apply(AVCodecContext *cc) const;
 	} video;
 
@@ -93,6 +116,8 @@ public:
 		std::vector<char> extradata;
 
 		audio_properties();
+		/* Apply these properties to an allocated AVCodecContext instance,
+		 * suitable for creating encoders, decoders, etc. */
 		void apply(AVCodecContext *cc) const;
 	} audio;
 
@@ -126,8 +151,8 @@ private:
  *
  * The contents of a stream_packet are entirely situation-dependant.
  * It may contain encoded or decoded video or audio in any format, or
- * any other type as needed. Refer to the source of the packet for
- * usage.
+ * any other type as needed. Refer to the source of the packet (probably
+ * an input_device) for usage.
  *
  * A stream_packet instance is not threadsafe, but the underlying data
  * is. You may have stream_packet instances referring to the same data
@@ -143,16 +168,30 @@ public:
 		MotionFlag = 0x2
 	};
 
+	/* These fields are not shared between stream_packet instances, and
+	 * thus are safe to modify, but only will apply to packets copied from
+	 * this instance after modification. */
+
+	/* Size in bytes of the data field */
 	unsigned size;
+	/* Flags, as a bitmask of values from Flags */
 	unsigned flags;
-	/* PTS is normalized to AV_TIME_BASE_Q and synchronized across media. */
+	/* Presentation timestamp of this packet, which is normalized during creation
+	 * of the packet to AV_TIME_BASE_Q, and synchronized across related media streams.
+	 * Should increase monotonically, excluding overflow, but should not be assumed
+	 * to be consistent after a change to stream_properties. */
 	int64_t  pts;
-	int      type; // AVMEDIA_TYPE_VIDEO or AVMEDIA_TYPE_AUDIO
-	time_t   ts_clock; // Used for prerecord event start time
-	/* XXX: Monotonic clock time when this packet was captured. This
-	 * is used in place of PTS for buffer management (see
-	 * stream_keyframe_buffer), but should be deprecated once we have
-	 * reliable PTS values to use. */
+	/* AVMEDIA_TYPE_VIDEO or AVMEDIA_TYPE_AUDIO */
+	int      type;
+	/* Wallclock time at packet capture. Do not compare this value between packets;
+	 * the wall clock is not reliable. Used for event recording times. */
+	time_t   ts_clock;
+	/* Monotonic clock time at packet capture. This is a reliable value for comparison
+	 * between capture of packets, but does not represent actual playback timing.
+	 *
+	 * Avoid use. Currently used in place of PTS for buffer management to avoid problems
+	 * with unreliable PTS values, but capture time does not in any way represent packet
+	 * time. */
 	time_t   ts_monotonic;
 	/* Sequence number of this packet, unique to the input that created
 	 * it. Should not be reset unless the handle is stopped and started
@@ -176,7 +215,7 @@ private:
 };
 
 /* Simple FIFO-style queue of stream_packet instances. The buffer is
- * unbounded and should be used directly only with great care.
+ * unbounded and should only be used directly with great care.
  * Refer to subclasses for implementations with limits on the size of
  * the buffer. */
 class stream_buffer : public std::deque<stream_packet>
@@ -195,15 +234,25 @@ public:
 protected:
 	virtual void apply_bound();
 
+	/* Prevent use of direct modification functions */
 	using std::deque<stream_packet>::insert;
 	using std::deque<stream_packet>::push_back;
 	using std::deque<stream_packet>::push_front;
 };
 
-/* Implementation of stream_buffer which always begins with a video
- * keyframe, and optionally keeps at least enough frames to cover a
- * given duration. Frames are dropped from the beginning when the
- * next keyframe can satisfy the duration requirement.
+/* Implementation of stream_buffer which automatically drops packets
+ * with regard to keyframes, to avoid corruption of the stream.
+ *
+ * Normally, the buffer will begin with one keyframe and contain any
+ * subsequent packets, and be overwritten with the next keyframe.
+ * If duration is set, the buffer will hold at least that many seconds
+ * of packets (by capture time), but still only drop before a keyframe.
+ *
+ * If enforce_keyframe is true (default), the first packet will always
+ * be a keyframe, and thus a safe place to start using the stream. Unset
+ * this if taking packets out of the buffer as they're processed.
+ *
+ * This may be woefully unsuitable for audio.
  */
 class stream_keyframe_buffer : public stream_buffer
 {
@@ -213,10 +262,7 @@ public:
 	/* Minimum buffer time in seconds, as counted on a monotonic
 	 * clock (stream_packet.ts_monotonic). The buffer will be trimmed
 	 * when the duration exceeds this value, but will always start
-	 * with a keyframe from at least duration seconds ago.
-	 *
-	 * XXX This should be refactored to use PTS once we have reliable
-	 * and consistent PTS values in stream_packet. */
+	 * with a keyframe from at least duration seconds ago. */
 	unsigned duration() const { return mDuration; }
 	void set_duration(unsigned duration);
 
