bluecherrydvr/bluecherry-apps

commit fb2c98583e44219b70b3c494c7c6b0b9aa9095de

Fix some variable signedness mismatches

server/rtsp.cpp
@@ -368,7 +368,7 @@ int rtsp_connection::readable()
 		return -1;
 	}
 
-	int rd = read(fd, rdbuf + rdbuf_len, sizeof(rdbuf) - rdbuf_len - 1);
+	unsigned int rd = read(fd, rdbuf + rdbuf_len, sizeof(rdbuf) - rdbuf_len - 1);
 	if (rd < 1)
 		return -1;
 
@@ -725,7 +725,7 @@ int rtsp_connection::handleSetup(rtsp_message &req)
 	rtsp_stream *stream = NULL;
 	int streamid = -1;
 	if (path.size() > 6 && path.compare(0, 6, "/live/") == 0) {
-		int sep = path.find('/', 6);
+		unsigned int sep = path.find('/', 6);
 		if (sep > 6) {
 			stream = rtsp_stream::findUri(path.substr(0, sep));
 			path = (sep+1 < path.size()) ? path.substr(sep+1) : std::string();
@@ -930,7 +930,7 @@ rtsp_stream *rtsp_stream::findUri(std::string uri)
 	/* for /live/ URLs, we ignore everything after the first two segments
 	 * to allow streamid and other parameters */
 	if (uri.size() > 6 && uri.compare(0, 6, "/live/") == 0) {
-		int p = uri.find('/', 6);
+		unsigned int p = uri.find('/', 6);
 		if (p != std::string::npos)
 			uri.erase(p);
 	}
@@ -981,11 +981,11 @@ void rtsp_stream::sessionActiveChanged(rtsp_session *session)
 		_activeSessionCount--;
 }
 
-void rtsp_stream::sendPackets(uint8_t *buf, int size, int flags)
+void rtsp_stream::sendPackets(uint8_t *buf, unsigned int size, int flags)
 {
 	pthread_mutex_lock(&sessions_lock);
 	bool first = true;
-	for (int p = 0; p+4 <= size; ) {
+	for (unsigned int p = 0; p+4 <= size; ) {
 		uint8_t *pkt    = buf + p;
 		uint32_t pkt_sz = AV_RB32(pkt);
 

lib/bc-core.cpp
@@ -27,7 +27,7 @@ static int rtsp_handle_init(struct bc_handle *bc, BC_DB_RES dbres)
 	const char *username, *password, *val, *port = "554";
 	char *device, *path, *t;
 	char url[1024];
-	int r;
+	unsigned int r;
 
 	bc->type = BC_DEVICE_RTP;
 

lib/bc-key.cpp
@@ -172,7 +172,7 @@ int bc_license_generate(char *dest, int dest_sz, int ndev)
 	if (dest_sz < 21)
 		return -EINVAL;
 
-	for (int i = 5; i < sizeof(raw); ++i)
+	for (unsigned int i = 5; i < sizeof(raw); ++i)
 		raw[i] = uint8_t(rand());
 
 	raw[2] = 'B' ^ raw[7];
@@ -318,7 +318,7 @@ end:
 int bc_read_licenses(std::vector<bc_license> &licenses)
 {
 	std::set<std::string> existingset, newset;
-	for (int i = 0; i < licenses.size(); i++)
+	for (unsigned int i = 0; i < licenses.size(); i++)
 		existingset.insert(licenses[i].license);
 
 	BC_DB_RES dbres = bc_db_get_table("SELECT * FROM Licenses");
@@ -363,7 +363,7 @@ int bc_read_licenses(std::vector<bc_license> &licenses)
 	bc_db_free_table(dbres);
 
 	// Anything that is not in newset is removed
-	for (int i = 0; i < licenses.size(); ) {
+	for (unsigned int i = 0; i < licenses.size(); ) {
 		if (newset.find(licenses[i].license) == newset.end())
 			licenses.erase(licenses.begin() + i);
 		else

server/rtsp.h
@@ -77,7 +77,7 @@ private:
 	rtsp_server * const server;
 	const int fd;
 	char rdbuf[10240];
-	int rdbuf_len;
+	unsigned int rdbuf_len;
 	pthread_mutex_t write_lock;
 	rtsp_write_buffer *wrbuf, *wrbuf_tail;
 
@@ -115,7 +115,7 @@ public:
 	void sessionActiveChanged(rtsp_session *session);
 
 	int activeSessionCount() { return _activeSessionCount; } 
-	void sendPackets(uint8_t *buf, int bufsz, int flags);
+	void sendPackets(uint8_t *buf, unsigned int bufsz, int flags);
 
 	/* Invoked to delete dead streams on the RTSP thread after a (thread-safe) remove() */
 	static void collectGarbage();

server/streaming.cpp
@@ -70,7 +70,6 @@ error:
 void bc_streaming_destroy(struct bc_record *bc_rec)
 {
 	AVFormatContext *ctx = bc_rec->stream_ctx;
-	int i;
 
 	if (!ctx)
 		return;
@@ -82,7 +81,7 @@ void bc_streaming_destroy(struct bc_record *bc_rec)
 		av_free(buf);
 	}
 
-	for (i = 0; i < ctx->nb_streams; ++i) {
+	for (unsigned int i = 0; i < ctx->nb_streams; ++i) {
 		avcodec_close(ctx->streams[i]->codec);
 		av_freep(&ctx->streams[i]->codec);
 		av_freep(&ctx->streams[i]);

utils/ptzcmd.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 	long id;
 	int delay, pan_speed, tilt_speed, pset_id;
 	char *cmd_str;
-	int ret, i;
+	int ret;
 	unsigned int cmd;
 	BC_DB_RES dbres;
 
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
 	bc_ptz_check(&bch, dbres);
 	bc_db_free_table(dbres);
 
-        for (i = 0, cmd = 0; i < strlen(cmd_str); i++) {
+        for (unsigned int i = 0, cmd = 0; i < strlen(cmd_str); i++) {
 		switch (cmd_str[i]) {
 		case 'S': cmd |= BC_PTZ_CMD_STOP; break;
 		case 'r': cmd |= BC_PTZ_CMD_RIGHT; break;

commit efb788765638556dc4892c83c3d3bc8ea7cd1769

server: rtsp: Fix comparison error
Introduced in fb2c98583e44219b70b3c494c7c6b0b9aa9095de, due to the sharing
of the rd variable.

server/rtsp.cpp
@@ -375,16 +375,16 @@ int rtsp_connection::readable()
 
 	rdbuf_len += rd;
 
-	rd = parse();
-	if (rd < 0)
+	int parsed = parse();
+	if (parsed < 0)
 		return -1;
 
-	if (rd) {
-		if (rd < rdbuf_len)
-			memmove(rdbuf, rdbuf + rd, rdbuf_len - rd);
-		if (rd > rdbuf_len)
-			rd = rdbuf_len;
-		rdbuf_len -= rd;
+	if (parsed) {
+		if (parsed < rdbuf_len)
+			memmove(rdbuf, rdbuf + parsed, rdbuf_len - parsed);
+		if (parsed > rdbuf_len)
+			parsed = rdbuf_len;
+		rdbuf_len -= parsed;
 	}
 
 	return 0;