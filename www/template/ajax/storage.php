8bca01be81c98458a7c18fd7d7e877852baa088f
ruminsam <ruminsam@corp.bluecherry.net>
2013-09-05 23:56:02 +0800

    display the record time on the web

--- www/template/ajax/storage.php
+++ www/template/ajax/storage.php
@@ -12,7 +12,10 @@ require('../template/template.lib.php');
 <form id='storageForm' method="post" action="/ajax/storage.php">
 <?php
 foreach ($locations as $id => $location){
-	print "<div id='separator'><label>".LOCATION."</label><INPUT name='path[]' type='text' id='path' class='path' value='{$location['path']}' style='width:270px;' /> Max:<INPUT style='width:40px;' type='text' name='max[]' id='max[]' value='".intval($location['max_thresh'])."' />%<div id='button' class='deleteShort'>".L_DELETE."</div></div>";
+	$recordTime = intval($location['record_time']);
+	$hours = floor($recordTime / 3600);
+	$minutues = floor(($recordTime % 3600) / 60);
+	print "<div id='separator'><label>".LOCATION."</label><INPUT name='path[]' type='text' id='path' class='path' value='{$location['path']}' style='width:200px;' />&nbsp;&nbsp;Max:<INPUT style='width:40px;' type='text' name='max[]' id='max[]' value='".intval($location['max_thresh'])."' />%&nbsp;&nbsp;&nbsp;Time:".$hours." h ".$minutues." m"."<div id='button' class='deleteShort'>".L_DELETE."</div></div>";
 }
 
 ?>
