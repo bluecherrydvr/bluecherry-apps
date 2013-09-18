d969970afe7dbe345da1822dc0d82d996e40f944
ruminsam <ruminsam@corp.bluecherry.net>
2013-09-19 00:32:44 +0700

    transcoding rtsp to mjpeg

--- php5/bluecherry.cpp
+++ php5/bluecherry.cpp
@@ -123,7 +123,6 @@ PHP_FUNCTION(bc_db_close)
 
 PHP_FUNCTION(bc_db_escape_string)
 {
-	zval *z_ctx;
 	char *str;
 	int str_len;
 	char *tmp_str, *tmp_str_cpy;
@@ -149,7 +148,6 @@ PHP_FUNCTION(bc_db_escape_string)
 
 PHP_FUNCTION(bc_db_query)
 {
-	zval *z_ctx;
 	char *sql;
 	int sql_len;
 
@@ -171,7 +169,6 @@ PHP_FUNCTION(bc_db_last_insert_rowid)
 
 PHP_FUNCTION(bc_db_get_table)
 {
-	zval *z_ctx;
 	BC_DB_RES dbres;
 	char *sql;
 	int sql_len;
@@ -446,9 +443,7 @@ PHP_FUNCTION(bc_buf_size)
 PHP_FUNCTION(bc_buf_data)
 {
 	struct bc_handle *bch;
-        unsigned char *data;
-	int size;
-
+        
 	BCH_GET_RES("bc_buf_data");
 
 	const stream_packet &packet = bch->input->packet();
