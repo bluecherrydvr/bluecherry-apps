6da09bc01a8be4ae78d04c120bad1eb79e85f8c6
Ismael Luceno <ismael.luceno@corp.bluecherry.net>
2013-11-15 17:43:26 -0300

    misc: Remove libav in order to upgrade it with a git submodule

--- misc/libav/libavcodec/libavcodec.v
+++
@@ -1,8 +0,0 @@
-LIBAVCODEC_$MAJOR {
-        global: av*;
-                audio_resample;
-                audio_resample_close;
-                #deprecated, remove after next bump
-                img_get_alpha_info;
-        local:  *;
-};
