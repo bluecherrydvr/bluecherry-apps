8bc9e5fd185a8c3fc809e82b365d7cf717f5ff48
Andrey Utkin <andrey.utkin@corp.bluecherry.net>
2014-02-08 13:34:13 -0600

    server/motion_processor.cpp: fix frame init

--- server/motion_processor.cpp
+++ server/motion_processor.cpp
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <pthread.h>
+#include <assert.h>
 
 #include "bt.h"
 
@@ -113,11 +114,10 @@ void motion_processor::run()
 		avpkt.data  = const_cast<uint8_t*>(pkt.data());
 		avpkt.size  = pkt.size;
 
-		AVFrame frame;
-		avcodec_get_frame_defaults(&frame);
+		AVFrame *frame = av_frame_alloc();
 
 		int have_picture = 0;
-		int re = avcodec_decode_video2(decode_ctx, &frame, &have_picture, &avpkt);
+		int re = avcodec_decode_video2(decode_ctx, frame, &have_picture, &avpkt);
 		if (re < 0) {
 			bc_log(Info, "Decoding failed for motion processor");
 		} else if (have_picture) {
@@ -141,12 +141,13 @@ void motion_processor::run()
 			if (!skip) {
 				last_tested_pts = pkt.pts;
 				skip_count = 0;
-				int re = detect(&frame);
+				int re = detect(frame);
 				if (re == 1)
 					pkt.flags |= stream_packet::MotionFlag;
 			}
 		}
 
+		av_frame_free(&frame);
 		output_source->send(pkt);
 
 		l.lock();
@@ -205,18 +206,14 @@ int motion_processor::detect(AVFrame *rawFrame)
 		fix_pix_fmt(rawFrame->format), rawFrame->width, rawFrame->height,
 		AV_PIX_FMT_GRAY8, SWS_BICUBIC, NULL, NULL, NULL);
 
-	/* XXX preallocated buffer? */
-	int bufSize = avpicture_get_size(AV_PIX_FMT_GRAY8, rawFrame->width, rawFrame->height);
-	uint8_t *buf = (uint8_t*)av_malloc(bufSize);
-	if (!buf)
-		return -1;
-
-	AVFrame frame;
-	avcodec_get_frame_defaults(&frame);
-	avpicture_fill((AVPicture*)&frame, buf, AV_PIX_FMT_GRAY8, rawFrame->width, rawFrame->height);
+	AVFrame *frame = av_frame_alloc();
+	frame->format = AV_PIX_FMT_GRAY8;
+	frame->width = rawFrame->width;
+	frame->height = rawFrame->height;
+	avpicture_alloc((AVPicture*)frame, (AVPixelFormat)frame->format, frame->width, frame->height);
 
 	sws_scale(convContext, (const uint8_t **)rawFrame->data, rawFrame->linesize, 0,
-		  rawFrame->height, frame.data, frame.linesize);
+		  rawFrame->height, frame->data, frame->linesize);
 
 #ifdef DEBUG_DUMP_MOTION_DATA
 	if (!md->dumpfile) {
@@ -233,11 +230,11 @@ int motion_processor::detect(AVFrame *rawFrame)
 	if (refFrame && refFrameHeight == rawFrame->height && refFrameWidth == rawFrame->width)
 	{
 		uint8_t *ref = refFrame->data[0];
-		uint8_t *cur = frame.data[0];
+		uint8_t *cur = frame->data[0];
 		uint8_t *val = 0;
 		uint8_t cv = 0; /* current value, for val[0] */
 		uint8_t lv = 0;
-		int w = frame.linesize[0], h = rawFrame->height;
+		int w = frame->linesize[0], h = rawFrame->height;
 		int total = w * h;
 		int x = 0, y = 0;
 		int threshold_cell_w = ceil(w/32.0);
@@ -335,19 +332,17 @@ int motion_processor::detect(AVFrame *rawFrame)
 		}
 
 		free(val);
-		av_free(buf);
 	} else {
 		if (refFrame) {
 			av_free(refFrame->data[0]);
 			av_free(refFrame);
 		}
-		refFrame = avcodec_alloc_frame();
-		/* Using sizeof(AVFrame) is forbidden, but we link against our own libav. */
-		memcpy(refFrame, &frame, sizeof(frame));
-		refFrameHeight = rawFrame->height;
-		refFrameWidth  = rawFrame->width;
+		refFrame = av_frame_clone(frame);
+		assert(refFrame);
 	}
 
+	avpicture_free((AVPicture*)frame);
+	av_frame_free(&frame);
 	return ret;
 }
 
