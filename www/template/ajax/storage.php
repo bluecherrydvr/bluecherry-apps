bluecherrydvr/bluecherry-apps

commit 96e429a9831d1563d2fd055df911a0c7e2b3bd6e

www: Display the available recording time estimation on the web

www/template/ajax/storage.php
@@ -7,12 +7,16 @@ require('../template/template.lib.php');
 <div id='saveButton'><?php echo SAVE; ?></div>
 <div class='bClear'></div>
 <div style="display:none;" id='tableEntry'>
-<?php		print "<label>".LOCATION."</label><INPUT name='path[]' type='text' id='path' class='path' value='' style='width:270px;' /> Max:<INPUT style='width:40px;' type='text' name='max[]' id='max[]' value='95' />% <div id='button' class='deleteShort'></div>"; ?>
+<?php		print "<div>".LOCATION."<INPUT name='path[]' type='text' id='path' class='path' value='' style='width:270px;margin-left:5px;' /> Max:<INPUT style='width:40px;margin-left:5px;' type='text' name='max[]' id='max[]' value='95' />% <div id='button' class='deleteShort'>".L_DELETE."</div></div>"; ?>
 </div>
 <form id='storageForm' method="post" action="/ajax/storage.php">
 <?php
 foreach ($locations as $id => $location){
-	print "<div id='separator'><label>".LOCATION."</label><INPUT name='path[]' type='text' id='path' class='path' value='{$location['path']}' style='width:270px;' /> Max:<INPUT style='width:40px;' type='text' name='max[]' id='max[]' value='".intval($location['max_thresh'])."' />%<div id='button' class='deleteShort'>".L_DELETE."</div></div>";
+	$recordTime = intval($location['record_time']);
+	$days = floor($recordTime / (3600 * 24));
+	$hours = floor(($recordTime % (3600* 24)) / 3600);
+	$minutues = floor(($recordTime % 3600) / 60);
+	print "<div id='separator'><div>".LOCATION."<INPUT name='path[]' type='text' id='path' class='path' value='{$location['path']}' style='width:270px; margin-left:5px;' /> Max:<INPUT style='width:40px; margin-left:5px;' type='text' name='max[]' id='max[]' value='".intval($location['max_thresh'])."' />%</div><div>Storage Estimation: ".$days." days ".$hours." hours and ".$minutues." minutues<div id='button' class='deleteShort' style='margin-top:-7px;'>".L_DELETE."</div></div></div>";
 }
 
 ?>

commit a9ac41d8e099cbeaa1368c37c62393bf34291d7f

Fix structure of Storage entries on the page, so that delete handler works again
There was a regression in commit 96e429a9831d1563d2fd055df911a0c7e2b3bd6e

www/template/ajax/storage.php
@@ -7,7 +7,7 @@ require('../template/template.lib.php');
 <div id='saveButton'><?php echo SAVE; ?></div>
 <div class='bClear'></div>
 <div style="display:none;" id='tableEntry'>
-<?php		print "<div>".LOCATION."<INPUT name='path[]' type='text' id='path' class='path' value='' style='width:270px;margin-left:5px;' /> Max:<INPUT style='width:40px;margin-left:5px;' type='text' name='max[]' id='max[]' value='94' />% <div id='button' class='deleteShort'>".L_DELETE."</div></div>"; ?>
+<?php		print "<label>".LOCATION."</label><INPUT name='path[]' type='text' id='path' class='path' value='' style='width:270px;margin-left:5px;' /> Max:<INPUT style='width:40px;margin-left:5px;' type='text' name='max[]' id='max[]' value='94' />% <div id='button' class='deleteShort'>".L_DELETE."</div>"; ?>
 </div>
 <form id='storageForm' method="post" action="/ajax/storage.php">
 <?php
@@ -16,7 +16,7 @@ foreach ($locations as $id => $location){
 	$days = floor($recordTime / (3600 * 24));
 	$hours = floor(($recordTime % (3600* 24)) / 3600);
 	$minutues = floor(($recordTime % 3600) / 60);
-	print "<div id='separator'><div>".LOCATION."<INPUT name='path[]' type='text' id='path' class='path' value='{$location['path']}' style='width:270px; margin-left:5px;' /> Max:<INPUT style='width:40px; margin-left:5px;' type='text' name='max[]' id='max[]' value='".intval($location['max_thresh'])."' />%</div><div>Storage Estimation: ".$days." days ".$hours." hours and ".$minutues." minutues<div id='button' class='deleteShort' style='margin-top:-7px;'>".L_DELETE."</div></div></div>";
+	print "<div id='separator'><div>".LOCATION."<INPUT name='path[]' type='text' id='path' class='path' value='{$location['path']}' style='width:270px; margin-left:5px;' /> Max:<INPUT style='width:40px; margin-left:5px;' type='text' name='max[]' id='max[]' value='".intval($location['max_thresh'])."' />%</div><div>Storage Estimation: ".$days." days ".$hours." hours and ".$minutues." minutues</div><div id='button' class='deleteShort' style='margin-top:-32px;'>".L_DELETE."</div></div>";
 }
 
 ?>