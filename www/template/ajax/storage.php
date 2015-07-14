92efce7c82817d4c79480c917f4a29878264353f
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2015-07-14 22:51:44 +0300

    Fix structure of Storage entries on the page, so that delete handler works again
    There was a regression in commit 96e429a9831d1563d2fd055df911a0c7e2b3bd6e

--- www/template/ajax/storage.php
+++ www/template/ajax/storage.php
@@ -7,7 +7,7 @@ require('../template/template.lib.php');
 <div id='saveButton'><?php echo SAVE; ?></div>
 <div class='bClear'></div>
 <div style="display:none;" id='tableEntry'>
-<?php		print "<div>".LOCATION."<INPUT name='path[]' type='text' id='path' class='path' value='' style='width:270px;margin-left:5px;' /> Max:<INPUT style='width:40px;margin-left:5px;' type='text' name='max[]' id='max[]' value='94' />% <div id='button' class='deleteShort'>".L_DELETE."</div></div>"; ?>
+<?php		print "<label>".LOCATION."</label><INPUT name='path[]' type='text' id='path' class='path' value='' style='width:270px;margin-left:5px;' /> Max:<INPUT style='width:40px;margin-left:5px;' type='text' name='max[]' id='max[]' value='94' />% <div id='button' class='deleteShort'>".L_DELETE."</div>"; ?>
 </div>
 <form id='storageForm' method="post" action="/ajax/storage.php">
 <?php
@@ -16,7 +16,7 @@ foreach ($locations as $id => $location){
 	$days = floor($recordTime / (3600 * 24));
 	$hours = floor(($recordTime % (3600* 24)) / 3600);
 	$minutues = floor(($recordTime % 3600) / 60);
-	print "<div id='separator'><div>".LOCATION."<INPUT name='path[]' type='text' id='path' class='path' value='{$location['path']}' style='width:270px; margin-left:5px;' /> Max:<INPUT style='width:40px; margin-left:5px;' type='text' name='max[]' id='max[]' value='".intval($location['max_thresh'])."' />%</div><div>Storage Estimation: ".$days." days ".$hours." hours and ".$minutues." minutues<div id='button' class='deleteShort' style='margin-top:-7px;'>".L_DELETE."</div></div></div>";
+	print "<div id='separator'><div>".LOCATION."<INPUT name='path[]' type='text' id='path' class='path' value='{$location['path']}' style='width:270px; margin-left:5px;' /> Max:<INPUT style='width:40px; margin-left:5px;' type='text' name='max[]' id='max[]' value='".intval($location['max_thresh'])."' />%</div><div>Storage Estimation: ".$days." days ".$hours." hours and ".$minutues." minutues</div><div id='button' class='deleteShort' style='margin-top:-32px;'>".L_DELETE."</div></div>";
 }
 
 ?>
