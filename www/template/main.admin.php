3336656d43bc9bd41cab32017f0dcc8e07392f8e
Aleksandern <aleksandern08@gmail.com>
2015-08-26 16:29:53 +0000

    template modifications

--- www/template/main.admin.php
+++ www/template/main.admin.php
@@ -1,99 +1,270 @@
 <?php defined('INDVR') or exit(); 
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
+<html lang="en">
 
-<html>
 <head>
-	<title><?php echo DVR_COMPANY_NAME.' '.DVR_DVR.PAGE_HEADER_SEPARATOR.PAGE_HEADER_MAIN; ?></title>
-	<link rel="stylesheet" href="template/styles.css">
-	<link rel="stylesheet" href="template/datepicker.css">
-	<script type="text/javascript" src="./lib/mootools.js"></script>
-	<script type="text/javascript" src="./lib/mootools-more.js"></script>
-	<script type="text/javascript" src="./lib/mootools-plugins.js"></script>
-	<script type="text/javascript" src="./lib/js/main.js"></script>
-	<script type="text/javascript" src="./lib/js/var.php"></script>
-	<script>
-		window.addEvent('load', function(){ 
-			
-			containerDivID = 'pageContainer';
-			var mainMenu = new DVRmainMenu();
-			var pageToOpen = (Cookie.read('currentPage') || 'news');
-			var pageToOpenData = (Cookie.read('currentPageData') || '');	
-			var openPage = new DVRPage(pageToOpen, pageToOpenData);
-			<?php
-				echo (!globalSettings::getParameter('G_DISABLE_WEB_STATS')) ? "updateStatData();" : "";
-			?>
-			
-		});
-	</script>
-</head>
-<body>
-	<div id="leftColumn">
-		<div id="logo"></div>
-		<div id="logout">[ <a href="/ajax/logout.php"><?php echo LOGOUT; ?></a> ]</div>
-		<div id="mainMenu">
-	  		<ul id="menuButtons">
-				<li class='liveView'><?php echo MMENU_LIVEVIEW; ?></li>
-				<li class='downloadClient'><?php echo MMENU_CLIENT_DOWNLOAD; ?></li>
-				<hr>
-				<li id="news" class="news"><?php echo MMENU_NEWS; ?></li>
-				<hr>
-				<li id="general" class="settingsGeneral"><?php echo MMENU_GS; ?></li>
-				<li id="storage" class="settingsStorage"><?php echo MMENU_STORAGE; ?></li>
-				<li id="users" class="settingsUsers"><?php echo MMENU_USERS; ?></li>
-				<li id="activeusers" class="activeUsers"><?php echo MMENU_ACTIVE_USERS; ?></li>
-				<hr>
-				<li id="devices" class="devices"><?php echo MMENU_DEVICES; ?></li>
-				<li id="deviceschedule" class="globalschedule"><?php echo MMENU_SCHED; ?></li>
-				<li id="notifications" class="notifications"><?php echo MMENU_NOTFICATIONS; ?></li>
-				<hr>
-				<li id="statistics" class="eventStatistics"><?php echo MMENU_STATISTICS; ?></li>
-				<li id="backup" class="backup"><?php echo MMENU_BACKUP; ?></li>
-				<li id="log" class="viewLog"><?php echo MMENU_LOG; ?></li>
-				<li id="licensing" class="licensing"><?php echo MMENU_LICENSING; ?></li>
-			</ul>
-		</div>
-		<div id="sysMessages">
-			<div id="sr"><div id="message" class="OK"><?php echo SERVER_RUNNING; ?></div></div>
-			<div id="snr"><div id="message" class="F"><?php echo SERVER_NOT_RUNNING; ?></div></div>
-			<div id="ncn"><div id="message" class="F"><?php echo NO_CONNECTION; ?></div></div>
-			<div id="ftw"><div id="message" class="F"><?php echo WRITE_FAILED; ?><div id="writeFailTime"></div></div></div>
-			<?php
-				if ($global_settings->data['G_DISABLE_VERSION_CHECK']==0)
-				if (!$version->version['up_to_date']){
-					echo '<div id="version"><div id="message" class="INFO">'.NOT_UP_TO_DATE.'<br /><a id="lmNewVersion" href="#">'.WANT_TO_LEARN_MORE.'</a></div></div>';
-				} else {
-					echo '<div id="version"><div id="message" class="OK">'.UP_TO_DATE.': '.$version->version['installed'].'</div></div>';
-				};
-			?>
-			<div class='bClear'></div>
-		</div>
-		<div id="serverStats">
-			<div><?php echo STATS_HEARDER; ?></div><HR>
-			<div class="label"><?php echo STATS_CPU; ?></div>
-			<div class="progressBar" id='cpu'>
-				<div class="text">0</div>
-			</div>
-			<div class="label"><?php echo STATS_MEM; ?></div>
-			<div class="progressBar" id='mem'>
-				<div class="text">0</div>
-			</div>
-			<div class="label" id="meminfo"></div>
-			<div class="label"><?php echo STATS_UPT ?></div>
-			<div id="serverUptime"></div>
-		</div>
-	</div>
-	<div id="centerColumn">
-		<?php
-			if (!empty($_GLOBALS['general_error'])){
-				echo "<div id='generalMessage'><div id='message' class='{$_GLOBALS['general_error']['type']}'>".$_GLOBALS['general_error']['text']."</div></div>";
-			}
-		?>
-		<div id="pageContainer">
 
-		</div>
-	</div>
-	<div id="onLoadFailureContainer"><div id="onLoadFailure"><h1><?php echo COUND_NOT_OPEN_PAGE;  ?></h1><p><?php echo COUND_NOT_OPEN_PAGE_EX; ?></p></div></div>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo DVR_COMPANY_NAME; ?>">
+    <meta name="author" content="<?php echo DVR_COMPANY_NAME; ?>">
+
+	<title><?php echo DVR_COMPANY_NAME.' '.DVR_DVR.PAGE_HEADER_SEPARATOR.PAGE_HEADER_MAIN; ?></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/template/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="/template/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="/template/dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/template/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="/template/bower_components/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="/template/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
+    <!-- Custom CSS -->
+    <link href="/template/dist/css/main.css?v=<?php echo time(); ?>" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <div id="wrapper">
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">
+                    <img style="max-width: 80px; margin-top: -10px;" src="/img/logo.png">
+                </a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="/ajax/logout.php"><i class="fa fa-sign-out fa-fw"></i> <?php echo LOGOUT; ?></a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse" id="navbar-collapse">
+                    <ul class="nav" id="side-menu">
+
+
+                        <li>
+                            <a href="javascript:void(0);" class='liveView'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_LIVEVIEW; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='downloadClient'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_CLIENT_DOWNLOAD; ?></a>
+                        </li>
+
+                        <li class="divider"></li>
+                        <li class="divider"></li>
+
+                        <li>
+                            <a href="/ajax/news.php" class="ajax-content"><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_NEWS; ?></a>
+                        </li>
+
+                        <li class="divider"></li>
+                        <li class="divider"></li>
+
+                        <li>
+                            <a href="javascript:void(0);" class='settingsGeneral'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_GS; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='settingsStorage'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_STORAGE; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='settingsUsers'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_USERS; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='activeUsers'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_ACTIVE_USERS; ?></a>
+                        </li>
+
+                        <li class="divider"></li>
+                        <li class="divider"></li>
+
+                        <li>
+                            <a href="javascript:void(0);" class='devices'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_DEVICES; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='globalschedule'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_SCHED; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='notifications'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_NOTFICATIONS; ?></a>
+                        </li>
+
+                        <li class="divider"></li>
+                        <li class="divider"></li>
+
+
+                        <li>
+                            <a href="javascript:void(0);" class='eventStatistics'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_STATISTICS; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='backup'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_BACKUP; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='viewLog'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_LOG; ?></a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class='licensing'><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_LICENSING; ?></a>
+                        </li>
+                    </ul>
+
+                    <div class="panel-body">
+                        <div class="alert alert-success">
+                            <?php echo SERVER_RUNNING; ?>
+                        </div>
+
+                        <div class="alert alert-danger">
+                            <?php echo SERVER_NOT_RUNNING; ?>
+                        </div>
+
+                        <div class="alert alert-danger">
+                            <?php echo NO_CONNECTION; ?>
+                        </div>
+
+                        <div class="alert alert-danger">
+                            <?php echo WRITE_FAILED; ?>
+                        </div>
+
+
+
+			            <?php
+            				if ($global_settings->data['G_DISABLE_VERSION_CHECK']==0)
+            				if (!$version->version['up_to_date']){
+            					echo '<div class="alert alert-danger">'.NOT_UP_TO_DATE.'<br /><a id="lmNewVersion" href="#">'.WANT_TO_LEARN_MORE.'</a></div>';
+            				} else {
+            					echo '<div class="alert alert-success">'.UP_TO_DATE.': '.$version->version['installed'].'</div>';
+            				};
+            			?>
+                    </div>
+
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-bar-chart-o fa-fw"></i> <?php echo STATS_HEARDER; ?>
+                        </div>
+                        <div class="panel-body">
+                            <div>
+                                <p>
+                                    <strong><?php echo STATS_CPU; ?></strong>
+                                    <span class="pull-right text-muted">40%</span>
+                                </p>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                                        <span class="sr-only">40%</span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div>
+                                <p>
+                                    <strong><?php echo STATS_MEM; ?></strong>
+                                    <span class="pull-right text-muted">80%</span>
+                                </p>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                                        <span class="sr-only">80%</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <strong><?php echo STATS_UPT ?></strong>
+                            </div>
+                            <div>
+                                6 day(s), 18:20:52
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+
+        <?php  if (!empty($_GLOBALS['general_error'])){ ?>
+            <div class="row general-error">
+                <div class="col-lg-12">
+                    <div class="alert alert-warning <?php echo $_GLOBALS['general_error']['type']; ?>">
+                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                        <?php echo $_GLOBALS['general_error']['text']; ?>
+                    </div>
+                </div>
+            </div>
+		<?php } ?>
+
+        <div id="page-container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Title</h1>
+                </div>
+
+                <form method="POST" action="/ajax/news.php">
+                    <input type="hidden" name="type" value="123">
+                    <a href="" class="send-req-form">asd</a>
+                <form>
+            </div>
+        </div>
+
+        </div>
+
+    </div>
+
+    <!-- jQuery -->
+    <script src="/template/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/template/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="/template/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="/template/bower_components/raphael/raphael-min.js"></script>
+    <script src="/template/bower_components/morrisjs/morris.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="/template/dist/js/sb-admin-2.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+	<script type="text/javascript" src="/lib/js/var.php"></script>
+    <script src="/template/dist/js/main.js?v=<?php echo time(); ?>"></script>
 </body>
 </html>
