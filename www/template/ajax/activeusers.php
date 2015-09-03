b67146caa630daa98e81e660fbf8b47a457d25ea
Aleksandern <aleksandern08@gmail.com>
2015-09-03 11:45:21 +0000

    connected users page

--- www/template/ajax/activeusers.php
+++ www/template/ajax/activeusers.php
@@ -1,15 +1,57 @@
 <?php defined('INDVR') or exit(); ?>
 
-<h1 id="header" class="header"><?php echo ACTIVE_USERS_HEADER; ?></h1>
-<hr />
-<p><?php echo ACTIVE_USERS_SUBHEADER; ?></p>
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header"><?php echo ACTIVE_USERS_HEADER; ?>
 
-<?php
-		echo '<table id="active-user-table"><tr class="header"><th>'.USERS_TABLE_HEADER_NAME.'</th><th>'.AU_IP.'</th><th></th><th></th><th></th></tr>';
-		$f = false;
-		foreach($this->users as $k => $user){
-			echo "<tr class='".(($f) ? 'low' : 'high')."' id='{$user->info['id']}'><td>{$user->info['name']}</td><td>{$user->info['ip']}</td><td>".(($user->info['from_client']) ? RCLIENT : WEB)."</td><td class='".(($user->info['kick']) ? 'kicked' : "kick")."' id='{$user->info['ip']}'>".(($user->info['kick']) ? AU_KICKED : AU_KICK)."</td></tr>";
-			$f = !$f;
-		}
-		echo '</table>';
-?>
+        <ol class="breadcrumb">
+            <li class="active"><?php echo ACTIVE_USERS_SUBHEADER; ?></li>
+        </ol>
+        </h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <table class="table table-striped table-vert-align">
+            <thead>
+                <tr>
+                    <th><?php echo USERS_TABLE_HEADER_NAME; ?></th>        
+                    <th><?php echo AU_IP; ?></th>        
+                    <th></th>        
+                    <th></th>        
+                </tr>
+            </thead>
+            <tbody>
+            <?php 
+    	    	$f = false;
+        		foreach($this->users as $k => $user) {
+            ?>
+                <tr>
+                    <td><?php echo $user->info['name']; ?></td>        
+                    <td><?php echo $user->info['ip']; ?></td>        
+                    <td><?php echo (($user->info['from_client']) ? RCLIENT : WEB); ?></td>        
+                    <td>
+                        <?php if ($user->info['kick']) { ?>
+                            <a href="javascript:void(0);" class="btn btn-danger"><?php echo AU_KICKED; ?></a>
+                        <?php } else { ?>
+                        <form action="/ajax/update.php" method="POST">
+                            <input type="hidden" name="mode" value="kick">
+                            <input type="hidden" name="type" value="ActiveUsers">
+                            <input type="hidden" name="kick" value="1">
+                            <input type="hidden" name="id" value="<?php echo $user->info['ip']; ?>">
+
+                            <button type="submit" class="btn btn-warning send-req-form" data-loading-text="<?php echo AU_KICK; ?>"><?php echo AU_KICK; ?></button>
+                        </form>
+                        <?php } ?>
+                    </td>        
+                </tr>
+            <?php } ?>
+
+            <tbody>
+        </table>
+
+    </div>
+</div>
+
+
