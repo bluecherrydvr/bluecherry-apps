e54ea0dc41bcac21c6dbd4cec1bb305785b4060a
Aleksandern <aleksandern08@gmail.com>
2015-09-17 11:30:03 +0000

    backup page

--- www/template/ajax/backup.php
+++ www/template/ajax/backup.php
@@ -2,43 +2,78 @@
 #template common functions
 require('../template/template.lib.php');
 
-if (false){
-	?>
-	<?php
-	exit();
-}
 ?>
 
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header"><?php echo BACKUP_HEADER; ?>
+    
+        <ol class="breadcrumb">
+            <li class="active"> <?php echo BACKUP_SUBHEADER; ?></li>
+        </ol>
+        </h1>
+    </div>
+</div>
 
-<h1 id='header' class='header'><?php echo BACKUP_HEADER; ?></h1>
-<p><?php echo BACKUP_SUBHEADER; ?></p>
-<div class='container-separator' id='backup-container'>
-	<div class='title main'><?php echo BACKUP_B_TITLE; ?></div>
-	<form action='/ajax/backup.php?mode=prepare' method='post' id='backup-form'>
-		<div class='container-separator'>
-			<div class='title'><?php echo str_replace(array('%USERS%', '%DBNAME%'), array('root', $database_parameters['db']), BACKUP_PWD);?></div>
-			<input type='password' name='pwd'>
-		</div>
-		<div class='container-separator'>
-			<div class='title'><?php echo BACKUP_B_OPT; ?></div>
-			<div><input type='checkbox' name='noevents' id='no-events' checked><?php echo BACKUP_B_OPT_NE; ?></div>
-			<div><input type='checkbox' name='nodevices' id='no-devices'><?php echo BACKUP_B_OPT_ND; ?></div>
-			<div><input type='checkbox' name='nousers' id='no-users'><?php echo BACKUP_B_OPT_NU; ?></div>
-		</div>
-		<div><input type='submit' value='<?php echo U_BACKUP; ?>' id='backup-form-submt'></div>
-	</form>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading"><?php echo BACKUP_B_TITLE; ?></div>
+            <div class="panel-body">
+                <form action="/ajax/backup.php?mode=prepare" method="post" id="backup-form">
+                    <div class="form-group">
+                        <label class=""><?php echo str_replace(array('%USERS%', '%DBNAME%'), array('root', $database_parameters['db']), BACKUP_PWD); ?></label>
+                        <input class="form-control" type="password" name="pwd"  />
+                    </div>
+                    <div class="form-group">
+                        <label class=""><?php echo BACKUP_B_OPT; ?></label>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="noevents" checked />
+                                <?php echo BACKUP_B_OPT_NE; ?>
+                            </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="nodevices" />
+                                <?php echo BACKUP_B_OPT_ND; ?>
+                            </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="nousers" />
+                                <?php echo BACKUP_B_OPT_NU; ?>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-success send-req-form" type="submit"> <?php echo U_BACKUP; ?></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
-<div class='container-separator' id='restore-container'>
-	<form action='' method='post' id='restore-form' enctype="multipart/form-data">
-	<div class='title main'><?php echo BACKUP_R_TITLE; ?></div>
-	<div class='container-separator' id='restore-pwd-container' style='display:none;'>
-		<div class='title'><?php echo str_replace(array('%USERS%', '%DBNAME%'), array('root', $database_parameters['db']), BACKUP_PWD);?></div>
-		<input type='password' name='pwd'>
-	</div>
-	<div class='container-separator' id='restore-file-container'>
-		<div class='title'><?php echo BACKUP_R_LABEL; ?></div>
-		<input type='file' name='restore' id='restore-file'>	
-	</div>
-	<div id='restore-form-button-container' style='display:none;'></form><input type='submit' value='<?php echo U_RESTORE; ?>' id='restore-form-submt'><input type='submit' value='<?php echo U_CANCEL; ?>' id='restore-form-cancel'></div>
-	
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading"><?php echo BACKUP_R_TITLE; ?></div>
+            <div class="panel-body">
+                <form action="/ajax/backup.php?mode=restore" method="post">
+                    <div class="form-group" style="display: none;">
+                        <input class="form-control" type="password" name="pwd"  />
+                    </div>
+                    <div class="form-group">
+                        <label class=""><?php echo BACKUP_R_LABEL; ?></label>
+		                <input type="file" name="restoreDataFile">	
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-success send-req-form" type="submit" data-func="backupCopyPass"> <?php echo U_RESTORE; ?></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+
