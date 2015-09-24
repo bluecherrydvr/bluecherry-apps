80f8b3047fea62b82f48141bd7432a05fa47771a
Anton <anton@corp.bluecherry.net>
2015-09-25 00:06:17 +0400

    completed database schema update for new motion configuration options, changed default values
    added execute permissions to db upgrade script
    
    fixed Windows end-of-lines in db schema upgrade script
    
    changed double quotes to single quotes in db schema
    
    changed DECIMAL to decimal in db schema

--- misc/sql/mysql-upgrade/2021000/add_columns_to_Devices_new_motion_detection.sh
+++ misc/sql/mysql-upgrade/2021000/add_columns_to_Devices_new_motion_detection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'ALTER TABLE Devices ADD `motion_algorithm` tinyint(1) DEFAULT "1" AFTER `motion_map`; ALTER TABLE Devices ADD `frame_downscale_factor` DECIMAL(2,1) DEFAULT "0.1" AFTER `motion_algorithm`; ALTER TABLE Devices ADD `min_motion_area` smallint(6) DEFAULT "50" AFTER `frame_downscale_factor`;' | mysql -D"$dbname" -u"$user" -p"$password"
+echo 'ALTER TABLE Devices ADD `motion_algorithm` tinyint(1) DEFAULT "1" AFTER `motion_map`; ALTER TABLE Devices ADD `frame_downscale_factor` DECIMAL(2,1) DEFAULT "0.5" AFTER `motion_algorithm`; ALTER TABLE Devices ADD `min_motion_area` smallint(6) DEFAULT "5" AFTER `frame_downscale_factor`;' | mysql -D"$dbname" -u"$user" -p"$password"
 
 exit 0
 
