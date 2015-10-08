cf18f354bae72624ddc355847db1390413b86fdc
Andrey Utkin <andrey.utkin@corp.bluecherry.net>
2015-10-08 19:58:25 +0300

    Build fixing WIP

--- scripts/install_prereqs.sh
+++ scripts/install_prereqs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get install debhelper php5-dev ccache bison 
+apt-get install debhelper php5-dev ccache bison texinfo yasm cmake libbsd-dev libmysqlclient-dev libopencv-dev libudev-dev
