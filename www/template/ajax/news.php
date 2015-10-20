15f5422733ef0d7e954054e1d79ac396faa71562
Aleksandern <aleksandern08@gmail.com>
2015-10-20 15:22:41 +0000

    Announcements page

--- www/template/ajax/news.php
+++ www/template/ajax/news.php
@@ -1,18 +1,36 @@
 <?php defined('INDVR') or exit(); ?>
-<h1 class="header"><?php echo NEWS_HEADER; ?></h1><hr />
-<div id="dvrNews">
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header"><?php echo NEWS_HEADER; ?></h1>
+    </div>
+</div>
+<div class="row news-bl">
+    <div class="col-lg-12">
 <?php
 if (!$news->news) {
 	echo NO_NEWS;
 } else {
-	foreach($news->news as $entry){
- 	echo "
-	 <div class='news-entry' id=''>
- 		<div class='news-title'><a href='{$entry['link']}'>{$entry['title']}</a></div>
-		<div class='news-date'>".substr($entry['pubDate'], 0, 17)."</div>
-		<div class='news-text'>".str_replace("[...]", "[<a href='{$entry['link']}'>...</a>]", $entry['description'])."</div>
-	 </div >";
+    foreach($news->news as $entry){
+?>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4><a href="<?php echo $entry['link']; ?>" class="pull-left"><?php echo $entry['title']; ?></a></h4>
+
+            <span class="text-muted pull-right"><small><?php echo substr($entry['pubDate'], 0, 17); ?></small></span>
+
+            <div class="clearfix"></div>
+        </div>
+        <div class="panel-body">
+            <?php echo str_replace("[...]", "[<a href='{$entry['link']}'>...</a>]", $entry['description']); ?>
+        </div>
+        
+    </div>
+
+<?php    
 	};
 };
 ?>
+    </div>
 </div>
