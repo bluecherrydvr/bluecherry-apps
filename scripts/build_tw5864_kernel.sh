3e9649035834f2472cd50135e612051b7f8ef606
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2015-10-21 18:22:21 +0300

    Set kernel pkg maintainer info

--- scripts/build_tw5864_kernel.sh
+++ scripts/build_tw5864_kernel.sh
@@ -30,6 +30,8 @@ GIT_VERSION=`git describe --dirty --always --long`
 CFG_FILE=`ls -t /boot/config* | head -n1`
 cat $CFG_FILE <(echo "CONFIG_VIDEO_TW5864=m") > .config
 make olddefconfig
+export DEBEMAIL="maintainers@bluecherrydvr.com"
+export DEBFULLNAME="Bluecherry Maintainers"
 make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION="-bc-tw5864-$DISTRO-$ARCH"
 DST_DIR=$BASE_DIR/kernel_pkg/"$GIT_VERSION"/$DISTRO/$ARCH/
 mkdir -p "$DST_DIR"
