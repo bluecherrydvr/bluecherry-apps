789bbd545b320f96e1acec9f22c829361fda2785
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2015-10-28 14:36:29 +0200

    Add build_kernel_all_dist_arch.sh, plus some fixes

---
+++ scripts/build_kernel_all_dist_arch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+set -x
+
+# TODO Implement parallel building outside of sources tree
+for DIST in trusty wily wheezy jessie sid
+do
+	export DIST
+	for ARCH in amd64 i386
+	do
+		export ARCH
+		if [[ -e kernel_releases/${DIST}_${ARCH}.done ]]
+		then
+			continue
+		fi
+		echo "Doing for $DIST $ARCH"
+		`dirname $0`/build_kernel_dist_arch.sh
+		touch kernel_releases/${DIST}_${ARCH}.done
+	done
+done
+
--- scripts/build_tw5864_kernel.sh
+++ scripts/build_tw5864_kernel.sh
@@ -17,22 +17,31 @@ else
 	# Debian
 	DISTRO=`echo $VERSION | sed -e 's/^.*[(]//' -e 's/[)]//'`
 fi
-ARCH=`dpkg --print-architecture`
-BASE_DIR=`readlink -f .`
+DEBARCH=`dpkg --print-architecture`
+SCRIPTS_DIR=`dirname $0`
+BASE_DIR=`readlink -f $SCRIPTS_DIR/..`
 
 `dirname $0`/install_prereqs.sh
 sudo apt-get install -y -V bc libncurses-dev
 
 cd $KERN_SRC
 git clean -dxf
+git submodule update --recursive --init
 git submodule foreach --recursive "git clean -dxf"
 GIT_VERSION=`git describe --dirty --always --long`
 CFG_FILE=`ls -t /boot/config* | head -n1`
 cat $CFG_FILE <(echo -e "CONFIG_VIDEO_TW5864=m\nCONFIG_VIDEO_SOLO6X10=m") > .config
+unset ARCH
 make olddefconfig
 export DEBEMAIL="maintainers@bluecherrydvr.com"
 export DEBFULLNAME="Bluecherry Maintainers"
-make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION="-bc-tw5864-$DISTRO-$ARCH"
-DST_DIR=$BASE_DIR/kernel_pkg/"$GIT_VERSION"/$DISTRO/$ARCH/
+make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION="-bc-tw5864-$DISTRO-$DEBARCH"
+
+# Rebuild linux-image package properly, with our postinst
+PKG_GUTS_DIR="`pwd`/debian/tmp"
+cp $BASE_DIR/misc/kernel_pkg/inst_scripts/* $PKG_GUTS_DIR/DEBIAN/
+dpkg --build "$PKG_GUTS_DIR" ..
+
+DST_DIR=$BASE_DIR/kernel_releases/"$GIT_VERSION"/$DISTRO/$DEBARCH/
 mkdir -p "$DST_DIR"
 mv ../*.deb "$DST_DIR"
