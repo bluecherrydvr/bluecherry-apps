0073a6dc47c382d5de88936b0b1205eea28a23b2
Aleksandern <aleksandern08@gmail.com>
2015-11-09 06:51:31 +0000

    unable to connect to server from client fix

--- www/ajax/login.php
+++ www/ajax/login.php
@@ -6,10 +6,14 @@ $user_to_login = new user('username', $_POST['login']);
 $from_client = (empty($_POST['from_client'])) ? false : true;
 $result = $user_to_login->doLogin($_POST['password'], $from_client);
 
-if ($result == 'OK') {
-    Reply::ajaxDie('1', '/');
+if (Inp::post('from_client')) {
+    echo $result;
 } else {
-    Reply::ajaxDie('2', $result);
+    if ($result == 'OK') {
+        Reply::ajaxDie('1', '/');
+    } else {
+        Reply::ajaxDie('2', $result);
+    }
 }
 
 ?>
