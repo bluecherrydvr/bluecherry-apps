26a14c1642a97b997a02f27db90a030957b93e82
Aleksandern <aleksandern08@gmail.com>
2015-11-20 04:04:08 +0000

    media/request - changed permission

--- www/media/request.php
+++ www/media/request.php
@@ -9,7 +9,7 @@ include("../lib/lib.php");  #common functions
 
 
 $current_user = new user('id', $_SESSION['id']);
-$current_user->checkAccessPermissions('backup');
+$current_user->checkAccessPermissions('basic');
 session_write_close();
 #/auth check
 
