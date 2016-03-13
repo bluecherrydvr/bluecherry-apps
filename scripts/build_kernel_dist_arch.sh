9e4cefb62e632767d6e4599f28ab2570b4e54c69
Andrey Utkin <andrey.utkin@pb.com>
2016-03-13 16:44:54 +0200

    build_kernel_dist_arch.sh: ensure chroot is properly built before mounting over the kernel sources

--- scripts/build_kernel_dist_arch.sh
+++ scripts/build_kernel_dist_arch.sh
@@ -4,6 +4,15 @@ set -x
 
 : ${KERNEL_LOCATION:="`dirname $0`/../../linux"}
 : ${CHROOT_LOCATION:="`dirname $0`/../../chroots/${DIST}_${ARCH}"}
+
+# Trigger chroot building if it is not properly setup
+if ! [[ -e "${CHROOT_LOCATION}.setup" ]]
+then
+	export COMMAND=":"
+	export DIST ARCH
+	`dirname $0`/exec_dist_arch.sh
+fi
+
 KERNEL_LOCATION_IN_CHROOT=/usr/local/src/linux
 mkdir -p $CHROOT_LOCATION/$KERNEL_LOCATION_IN_CHROOT
 
