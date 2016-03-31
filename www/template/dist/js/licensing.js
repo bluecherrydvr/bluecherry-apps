ed6716f7ed15fb346f4ccce9797ad9b5721505f0
Aleksandern <aleksandern08@gmail.com>
2016-03-31 08:46:10 +0000

    the licenses keys page - reloading the page after a license key added. issue #298

--- www/template/dist/js/licensing.js
+++ www/template/dist/js/licensing.js
@@ -6,6 +6,9 @@ function licensingAdd(form, msg) {
     if (msg.status == 'CONF') {
         $('#licensing-conf').show();
         $('#licensing-machine-id').html(msg.msg);
+    } else if (msg.status == 6) {
+        var ajax_req = new ajaxReq();
+        ajax_req.ajaxContent('/licenses');
     }
 }
 
