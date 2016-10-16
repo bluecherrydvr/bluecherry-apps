0a43d47c027cfd6349ec9caee49991586051824a
Anton <anton@corp.bluecherry.net>
2016-10-16 22:47:19 +0400

    implemented wrapper for bc_license_machine_id()

--- www/lib/bc_license_wrapper.php
+++ www/lib/bc_license_wrapper.php
@@ -2,10 +2,27 @@
 
 function bc_license_machine_id()
 {
+	$output = array();
+	$ret = 0;
+	exec("/usr/lib/bluecherry/licensecmd bc_license_machine_id", $output, $ret);
+
+	if ($ret != 0)
+		return false;
+
+	return $output[0];
 }
 
 function bc_license_check($key)
 {
+	$output = array();
+	$ret = 0;
+
+	exec("/usr/lib/bluecherry/licensecmd bc_license_check ".$key, $output, $ret);
+
+	if ($ret != 0)
+		return false;
+
+	return $output[0];
 }
 
 function bc_license_check_auth($key, $auth)
