ce87cf1a41f018e17834f3adc706f62332c604b5
Anton <anton@corp.bluecherry.net>
2016-11-05 21:19:37 +0400

    fixed possible database upgrade failure

--- misc/sql/mysql-upgrade/2070700/modify_columns.sh
+++ misc/sql/mysql-upgrade/2070700/modify_columns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "ALTER TABLE EventTypesCam  MODIFY \`id\` varchar(255) NOT NULL;\
+echo "SET FOREIGN_KEY_CHECKS=0;ALTER TABLE EventTypesCam  MODIFY \`id\` varchar(255) NOT NULL;\
 ALTER TABLE EventTypesSys MODIFY \`id\` varchar(255) NOT NULL;\
 ALTER TABLE TagNames MODIFY \`name\` varchar(255) DEFAULT NULL;\
 ALTER TABLE ipPtzCommandPresets MODIFY \`driver\` varchar(12) DEFAULT '', MODIFY \`stop_zoom\` varchar(128) DEFAULT '' " | mysql -D"$dbname" -u"$user" -p"$password"
