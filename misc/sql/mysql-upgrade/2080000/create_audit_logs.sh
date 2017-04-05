03c22295af90396f54fb4f22cde2405cac2cf006
Anton <anton@corp.bluecherry.net>
2017-04-05 21:11:02 +0400

    fill AuditEventTypes table in db upgrade script

--- misc/sql/mysql-upgrade/2080000/create_audit_logs.sh
+++ misc/sql/mysql-upgrade/2080000/create_audit_logs.sh
@@ -23,3 +23,17 @@ CREATE TABLE `AuditLogs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ' | mysql -D"$dbname" -u"$user" -p"$password"
 
+echo '
+INSERT INTO AuditEventTypes VALUES (1, "Failed login attempt");
+INSERT INTO AuditEventTypes VALUES (2, "User logged in");
+INSERT INTO AuditEventTypes VALUES (3, "User logged out");
+INSERT INTO AuditEventTypes VALUES (4, "Device configuration changed");
+INSERT INTO AuditEventTypes VALUES (5, "New device added");
+INSERT INTO AuditEventTypes VALUES (6, "Device disabled");
+INSERT INTO AuditEventTypes VALUES (7, "Device deleted");
+INSERT INTO AuditEventTypes VALUES (8, "Password changed for user");
+INSERT INTO AuditEventTypes VALUES (9, "General settings changed");
+INSERT INTO AuditEventTypes VALUES (10, "Global schedule changed");
+INSERT INTO AuditEventTypes VALUES (11, "Email notification settings changed");
+' | mysql -D"$dbname" -u"$user" -p"$password"
+
