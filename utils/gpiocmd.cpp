6eed2489789ab1212e0c936cac502127cba73319
Anton <anton@corp.bluecherry.net>
2017-07-11 17:12:44 +0400

    utils/gpiocmd - implemented pin read and write

--- utils/gpiocmd.cpp
+++ utils/gpiocmd.cpp
@@ -27,6 +27,7 @@ int main(int argc, char **argv)
 {
         const char *name, *arg1 = NULL, *arg2 = NULL;
 	FILE *f;
+	int pin_number;
 
         if (argc < 2)
         {
@@ -66,6 +67,82 @@ int main(int argc, char **argv)
 			}
 
 			fclose(f);
+
+			break;
+		}
+
+		if (strcmp(name, "write") == 0)
+		{
+			char pathbuf[64];
+
+			if (!arg1 || !arg2)
+			{
+				fputs("pin number and value are requred\n", stderr);
+				return -1;
+			}
+
+			if (arg2[0] != '0' && arg2[0] != '1')
+			{
+				fputs("invalid value\n", stderr);
+				return -1;
+			}
+
+			pin_number = atoi(arg1);
+
+			sprintf(pathbuf, "/sys/class/gpio/gpio%d/value", pin_number);
+
+			f = fopen(pathbuf, "w");
+
+			if (!f)
+                        {
+                                perror("write pin");
+                                return -1;
+                        }
+
+                        if (!fwrite(arg2, 1, 1, f))
+                        {
+                                perror("write pin");
+                                return -1;
+                        }
+
+                        fclose(f);
+
+			break;
+		}
+
+		if (strcmp(name, "read") == 0)
+		{
+                        char pathbuf[64];
+			char valbuf[4];
+
+                        if (!arg1)
+                        {
+                                fputs("pin number is requred\n", stderr);
+                                return -1;
+                        }
+
+                        pin_number = atoi(arg1);
+
+                        sprintf(pathbuf, "/sys/class/gpio/gpio%d/value", pin_number);
+
+                        f = fopen(pathbuf, "r");
+
+                        if (!f)
+                        {
+                                perror("read pin");
+                                return -1;
+                        }
+
+                        if (!fread(valbuf, 1, 4, f))
+                        {
+                                perror("read pin");
+                                return -1;
+                        }
+
+			valbuf[1] = '\0';
+			puts(valbuf);
+
+                        fclose(f);
 
 			break;
 		}
