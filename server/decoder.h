89a877a4a740de3cb16a81ea0b6d884c9b3a5000
Anton <anton@corp.bluecherry.net>
2017-07-19 21:43:44 +0400

    implementing decoder class, work in progress

--- server/decoder.h
+++ server/decoder.h
@@ -7,8 +7,17 @@
 #ifndef __LIBBLUECHERRY_DECODER_H
 #define __LIBBLUECHERRY_DECODER_H
 
+#include "stream_elements.h"
+
 class decoder
 {
+public:
+	decoder(const stream_properties &prop);
+	~decoder();
+
+private:
+	struct AVCodecContext *decoder_ctx;
+	std::shared_ptr<const stream_properties> saved_properties;
 };
 
 #endif
