618cc99e66add11a025b7069198faba1d9ba8edf
Anton <anton@corp.bluecherry.net>
2017-07-19 17:53:04 +0400

    new classes reencoder, encoder, decoder

---
+++ server/reencoder.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017 Bluecherry, LLC
+ *
+ * Confidential, all rights reserved. No distribution is permitted.
+ */
+
+#ifndef __LIBBLUECHERRY_REENCODER_H
+#define __LIBBLUECHERRY_REENCODER_H
+
+#include "decoder.h"
+#include "encoder.h"
+
+class reencoder
+{
+};
+
+#endif
+
