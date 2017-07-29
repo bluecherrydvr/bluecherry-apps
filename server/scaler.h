a280d41e35c5ba040e7d1a647892a9a7548b36b3
Anton <anton@corp.bluecherry.net>
2017-07-29 20:12:50 +0400

    new class scaler, work in progress

---
+++ server/scaler.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2017 Bluecherry, LLC
+ *
+ * Confidential, all rights reserved. No distribution is permitted.
+ */
+
+#ifndef __LIBBLUECHERRY_SCALER_H
+#define __LIBBLUECHERRY_SCALER_H
+
+#include "stream_elements.h"
+
+extern "C"
+{
+#include <libavfilter/avfiltergraph.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/opt.h>
+}
+
+
+class scaler
+{
+public:
+	scaler();
+	~scaler();
+
+	bool init_scaler(int out_width, int out_height, AVBufferRef *hwframe_ctx);
+
+	void push_frame(AVFrame *in);
+	AVFrame *scaled_frame();
+
+private:
+	AVFilterContext *buffersink_ctx;
+	AVFilterContext *buffersrc_ctx;
+	AVFilterGraph *filter_graph;
+	AVBufferRef *hw_frame_ctx;
+};
+
+#endif
