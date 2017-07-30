27548a9dfb63f5552310107f467868c13e2d37ef
Anton <anton@corp.bluecherry.net>
2017-07-30 13:44:23 +0400

    add scaling step in reencoder

--- server/reencoder.cpp
+++ server/reencoder.cpp
@@ -2,7 +2,7 @@
 #include "vaapi.h"
 
 reencoder::reencoder()
-	: dec(0), enc(0),
+	: dec(0), enc(0), scl(0),
 	software_decoding(false), hwframe_ctx(0)
 {
 	bc_log(Info, "new reencoder instance created");
@@ -15,6 +15,8 @@ reencoder::~reencoder()
 		delete dec;
 	if (enc)
 		delete enc;
+	if (scl)
+		delete scl;
 }
 
 bool reencoder::push_packet(const stream_packet &packet)
@@ -31,6 +33,9 @@ bool reencoder::push_packet(const stream_packet &packet)
 bool reencoder::run_loop()
 {
 	AVFrame *frame;
+	AVFrame *scaled_frame;
+
+	/* 1) DECODE */
 	frame = dec->decoded_frame();
 
 	if (!frame)
@@ -39,11 +44,12 @@ bool reencoder::run_loop()
 	if (!frame)
 		return false;
 
-	if (!enc)
+	/* 2) SCALE */
+	if (!scl)
 	{
-		bc_log(Info, "creating encoder instance for reencoding");
+		bc_log(Info, "creating new scaler instance for reencoding");
 
-		enc = new encoder();
+		scl = new scaler();
 
 		const AVCodecContext *ctx = dec->get_ctx();
 
@@ -62,11 +68,12 @@ bool reencoder::run_loop()
 		else
 			hwframe_ctx = ctx->hw_frames_ctx;
 
-		//hardcoded parameters, for testing purposes
-		if (!enc->init_encoder(AVMEDIA_TYPE_VIDEO, AV_CODEC_ID_H264, 64000, frame->width/* /2 */, frame->height/* /2 */, hwframe_ctx))
+		//hardcoded output size
+		if (!scl->init_scaler(frame->width / 2, frame->height / 2, hwframe_ctx, ctx))
 		{
-			bc_log(Error, "Failed to initialize encoder instance");
-			delete enc;
+			bc_log(Error, "Failed to initialize scaler instance for reencoding");
+			delete scl;
+			scl = NULL;
 			return false;
 		}
 	}
@@ -77,6 +84,32 @@ bool reencoder::run_loop()
 		if (!vaapi_hwaccel::hwupload_frame(hwframe_ctx, frame))
 			return false;
 	}
+
+	scl->push_frame(frame);
+
+	scaled_frame = scl->scaled_frame();
+
+	if (scaled_frame == NULL)
+		return false;
+
+	/* 3) ENCODE */
+	if (!enc)
+	{
+		bc_log(Info, "creating encoder instance for reencoding");
+
+		enc = new encoder();
+
+
+		//hardcoded parameters, for testing purposes
+		if (!enc->init_encoder(AVMEDIA_TYPE_VIDEO, AV_CODEC_ID_H264, 64000, scaled_frame->width, scaled_frame->height, scaled_frame->hw_frames_ctx))
+		{
+			bc_log(Error, "Failed to initialize encoder instance");
+			delete enc;
+			enc = NULL;
+			return false;
+		}
+	}
+
 
 	enc->push_frame(frame);
 
