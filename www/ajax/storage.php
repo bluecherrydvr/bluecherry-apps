76141c41c2ada4e0083087a99c4efb4baa993d2a
Anton <anton@corp.bluecherry.net>
2018-01-03 18:42:11 +0400

    fix calculation of estimated storage time available

--- www/ajax/storage.php
+++ www/ajax/storage.php
@@ -14,13 +14,17 @@ class storage extends Controller {
 	    $locations = data::query("SELECT * FROM Storage");
         foreach(array_keys($locations) as $id) {
             $path = database::escapeString($locations[$id]['path']);
-            $fspace = disk_free_space($path) >> 20;
-            $query = "SELECT ({$fspace}) * period / size + period AS result FROM (".
+            $max_thresh = database::escapeString($locations[$id]['max_thresh']);
+            $available_space = (disk_free_space($path) - disk_total_space($path) * (1.0 - $max_thresh/100) ) >> 20;
+            if ($available_space < 0)
+                $available_space = 0;
+            $query = "SELECT ({$available_space}) * period / size AS result FROM (".
                   "SELECT SUM(CASE WHEN end < start THEN 0 ELSE end - start END".
                 ") period, SUM(size) DIV 1048576 size".
                 " FROM Media WHERE filepath LIKE '{$path}%') q";
     		$result = data::query($query);
     		$locations[$id]['record_time'] = $result[0]['result'];
+		$locations[$id]['used_percent'] = ceil((disk_total_space($path) - disk_free_space($path)) / disk_total_space($path) * 100);
         }
 
         $this->view->locations = $locations;
