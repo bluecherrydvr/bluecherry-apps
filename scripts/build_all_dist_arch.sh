be1359d95cbfdb7a876e7d831a21d1e28cf51356
Anton <anton@corp.bluecherry.net>
2018-05-29 10:07:43 -0500

    Add support for Ubuntu Bionic and Debian Stretch to 2.8 branch

--- scripts/build_all_dist_arch.sh
+++ scripts/build_all_dist_arch.sh
@@ -13,7 +13,7 @@ function work {
 }
 
 # TODO Implement parallel building outside of sources tree
-for DIST in trusty xenial jessie
+for DIST in bionic stretch trusty xenial jessie
 do
 	export DIST
 	for ARCH in amd64 i386
