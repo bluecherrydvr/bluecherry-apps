987f95945ecd6c5270fc42b46243e2a7386c4c37
Curtis Hall <curt@bluecherry.net>
2019-04-20 22:43:46 -0500

    Testing Sphinx youtube extension

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,10 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = []
-extensions = ['sphinxcontrib.youtube']
+
+sys.path.append(os.path.abspath('exts'))
+
+extensions = ['youtube']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
