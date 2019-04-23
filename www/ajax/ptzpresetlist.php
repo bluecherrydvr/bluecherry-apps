3812bfe066462a84dc195d6048c49c3a8dc2517e
Anton <anton@corp.bluecherry.net>
2019-04-23 22:01:15 +0400

    update license notice in PHP files

--- www/ajax/ptzpresetlist.php
+++ www/ajax/ptzpresetlist.php
@@ -1,11 +1,21 @@
 <?php
 
 /*
-    Copyright (c) 2010-2011 Bluecherry, LLC.
-    http://www.bluecherrydvr.com / support@bluecherrydvr.com
-    Confidential, all rights reserved. No distribution is permitted.
+ * Copyright 2010-2019 Bluecherry, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 class ptzpresetlist extends Controller {
 	
