cf43951403f347406a104803aaef4801714c33db
Mariusz Bialonczyk <manio@skyboo.net>
2019-12-03 09:41:58 +0100

    sql: make foreign keys to Devices/id as cascade
    This commit is changing the device_id references as cascade.
    
    This is allowing to reorder cameras internally using simple
    update, like e.g.:
    update Devices set id=5 where id=4;
    
    As the bluecherry server is not allowing (yet? ;)) to change
    this id (and thus related directory name/id in nvr structure),
    it is the easy way to reorder cameras without the need to
    delete it (and its settings) and re-add again.

---
+++ misc/sql/mysql-upgrade/3000002/make_device_id_fk_cascade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "alter table \`EventsCam\` drop foreign key \`EventsCam_ibfk_3\`;\
+alter table \`EventsCam\` add CONSTRAINT \`EventsCam_ibfk_3\` FOREIGN KEY (\`device_id\`) REFERENCES \`Devices\` (\`id\`) ON UPDATE CASCADE;\
+alter table \`Media\` drop foreign key \`Media_ibfk_1\`;\
+alter table \`Media\` add CONSTRAINT \`Media_ibfk_1\` FOREIGN KEY (\`device_id\`) REFERENCES \`Devices\` (\`id\`) ON UPDATE CASCADE;\
+alter table \`PTZPresets\` drop foreign key \`PTZPresets_ibfk_1\`;\
+alter table \`PTZPresets\` add CONSTRAINT \`PTZPresets_ibfk_1\` FOREIGN KEY (\`device_id\`) REFERENCES \`Devices\` (\`id\`) ON UPDATE CASCADE;" | mysql -D"$dbname" -u"$user" -p"$password"
+
+exit 0
+
