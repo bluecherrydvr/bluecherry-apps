51dfa02f9b193fd40cea8e6368b833babc63f75f
Jimmy <myjimmy@protonmail.com>
2021-05-15 02:00:42 +0300

    replace the RSS feeds with the embedded announceKit

--- www/template/main_admin.php
+++ www/template/main_admin.php
@@ -127,7 +127,7 @@
                         <li class="divider"></li>
 
                         <li>
-                            <a href="/news" class="ajax-content announcekit-widget" data-left-menu="true"><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_NEWS; ?>&nbsp;</a>
+                            <a href="/news" class="ajax-content" data-left-menu="true"><i class="fa fa-dashboard fa-fw"></i> <?php echo MMENU_NEWS; ?>&nbsp; <div class="visible-inline-block announcekit-widget"> </div></a>
                         </li>
 
                         <li class="divider"></li>
@@ -337,6 +337,12 @@
         window.announcekit.push({
             "widget": "https://announcekit.co/widgets/v2/SznKU",
             "selector": ".announcekit-widget"
+        })
+
+        window.announcekit.push({
+            "widget": "https://changelog.bluecherrydvr.com/widgets/v2/1fhia4",
+            "selector": ".announcekit-widget-embed",
+            "embed": true
         })
     </script>
     <script async src="https://cdn.announcekit.app/widget-v2.js"></script>
