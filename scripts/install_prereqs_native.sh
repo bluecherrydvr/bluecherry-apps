2b011b4e74c34fc7b28df02b23a8b43b9c228827
Anton <anton@corp.bluecherry.net>
2021-05-24 05:51:55 +0400

    add onvif_tool as a git submodule

--- scripts/install_prereqs_native.sh
+++ scripts/install_prereqs_native.sh
@@ -45,5 +45,5 @@ apt-get update
 
 apt-get upgrade -y -V
 
-apt-get install -y -V autoconf automake libtool git build-essential gcc g++ debhelper ccache bison flex texinfo yasm cmake libbsd-dev libopencv-dev libudev-dev libva-dev rsyslog sudo $ADDITIONAL_PKGS
+apt-get install -y -V autoconf automake libtool git build-essential gcc g++ debhelper ccache bison flex texinfo yasm cmake libbsd-dev libopencv-dev libudev-dev libva-dev rsyslog sudo gsoap libgsoap-dev $ADDITIONAL_PKGS
 
