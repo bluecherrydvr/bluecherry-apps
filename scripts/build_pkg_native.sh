600c54ec236e46616f01eac33f3c42a45a65af54
Curtis Hall <curt@bluecherry.net>
2022-05-05 19:37:48 -0500

    Comment out git submodule fetching
    Due to a rather annoying "feature" introduced in git there are errors thrown when fetching git submodules inside a docker container.  For now fetch submodules outside of the docker until this can be addressed better.

--- scripts/build_pkg_native.sh
+++ scripts/build_pkg_native.sh
@@ -22,8 +22,8 @@ fi
 # TODO Implement building outside of sources tree
 
 # Safety measures. TODO quick compilation without spare rebuilds
-git submodule foreach --recursive "git clean -dxf && git reset --hard"
-git submodule update --recursive --init
+#git submodule foreach --recursive "git clean -dxf && git reset --hard"
+#git submodule update --recursive --init
 
 echo "#define GIT_REVISION \"`git describe --dirty --always --long` `git describe --all`\"" > server/version.h
 
