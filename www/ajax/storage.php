a9f5fa99b2ef99bf27a8560ea3b8fc153665fa1e
Curtis Hall <chall@corp.bluecherry.net>
2024-01-01 20:00:49 -0600

    Revert "Update storage.php"
    This reverts commit e071449cfae7b366db1c6c43eee3faf16acc12b0.

--- www/ajax/storage.php
+++ www/ajax/storage.php
@@ -1,88 +1,113 @@
-<div class="row">
-    <div class="col-lg-12">
-        <h1 class="page-header"><?php echo STRAGE_HEADER; ?></h1>
-    </div>
-</div>
+<?php
 
-<div class="alert alert-warning"><i class="fa fa-warning fa-fw"></i>Note: <ul><li>We will automatically create a directory for you and assign the correct permissions.</li><li>Verify that the storage path you are using is mounted on start or the start of Bluecherry server.  Usually this is done in /etc/fstab.<li>If using SMB (CIFS) mount it is recommended to mount network storage with sync option and disabled cache (option cache=none for CIFS)</li><li>All storage paths must be added to either /mnt or /media</li></div>
+class storage extends Controller {
+
+    public function __construct(){
+        parent::__construct();
+        $this->chAccess('admin');
+    }
+
+    public function getData()
+    {
+        $this->setView('ajax.storage');
+
+        $locations = data::query("SELECT * FROM Storage");
+        foreach(array_keys($locations) as $id) {
+            $path = database::escapeString($locations[$id]['path']);
+            $max_thresh = database::escapeString($locations[$id]['max_thresh']);
+            $available_space = (disk_free_space($path) - disk_total_space($path) * (1.0 - $max_thresh/100) ) >> 20;
+            if ($available_space < 0)
+                $available_space = 0;
+            $query = "SELECT ({$available_space}) * period / size AS result FROM (".
+                  "SELECT SUM(CASE WHEN end < start THEN 0 ELSE end - start END".
+                ") period, SUM(size) DIV 1048576 size".
+                " FROM Media WHERE filepath LIKE '{$path}%') q";
+            $result = data::query($query);
+            $locations[$id]['record_time'] = $result[0]['result'];
+            $locations[$id]['used_percent'] = ceil((disk_total_space($path) - disk_free_space($path)) / disk_total_space($path) * 100);
+        }
+
+        $this->view->locations = $locations;
+    }
+
+    public function postData()
+    {
+        $storage_check = $this->ctl('storagecheck');
+
+        $values = Array();
+        foreach($_POST['path'] as $key => $path){
+            // Call the updated method from storagecheck
+            $dir_status = $storage_check->change_directory_permissions($path);
+            if ($dir_status[0] !== 'OK') {
+                // If directory permissions change failed, return the error
+                return data::responseJSON($dir_status[0], $dir_status[1]);
+            }
+
+            $max = intval($_POST['max'][$key]);
+            $min = $max - 10;
+            $values[] = "({$key}, '{$path}', {$max}, {$min})";
+        }
+
+        data::query("DELETE FROM Storage", true);
+
+        $status = true;
+        if (!empty($values)) {
+            $values = implode(',', $values);
+            $status = (data::query("INSERT INTO Storage (priority, path, max_thresh, min_thresh) VALUES {$values}", true)) ? $status : false;
+        }
+
+        data::responseJSON($status);
+    }
+}
 
 
+root@7007cams:/usr/share/bluecherry/www/ajax# cat storagecheck.php
+<?php
 
-<div class="row">
-    <div class="col-lg-12">
-        <div class="btn-group pull-right">
-            <a href="javascript:void(0);" class="btn btn-success" id="store-add-but"><i class="fa fa-plus fa-fw"></i>  <?php echo ADD_LOCATION; ?></a>
-            <a href="javascript:void(0);" class="btn btn-success send-req-form" data-form-id="storage-form"><i class="fa fa-check fa-fw"></i>  <?php echo SAVE_CHANGES; ?></a>
-        </div>
-        <div class="clearfix"></div>
-    </div>
-</div>
+class storagecheck extends Controller {
 
-<br>
+    public function __construct()
+    {
+        parent::__construct();
+        $this->chAccess('admin');
+    }
 
-<form id="storage-form" action="/storage" method="POST">
-    <div class="row">
-        <div class="col-lg-12">
-            <table class="table table-striped table-storage">
-                <tbody>
-                <?php foreach ($locations as $id => $location) {
-                    $recordTime = intval($location['record_time']);
-                    $used_storage = intval($location['used_percent']);
-                    $days = floor($recordTime / (3600 * 24));
-                    $hours = floor(($recordTime % (3600 * 24)) / 3600);
-                    $minutes = floor(($recordTime % 3600) / 60);
-                ?>
-                    <tr class="table-storage-tr-data">
-                        <td class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                            <label class="control-label"><?php echo LOCATION; ?></label>
-                        </td>
-                        <td class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                            <input class="form-control storage-path" name="path[]" type="text" value="<?php echo $location['path']; ?>" />
-                        </td>
-                        <td class="col-lg-1 col-md-1 col-sm-1 col-xs-1 align-right">
-                            <label class="control-label">Max:</label>
-                        </td>
-                        <td class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                            <div class="input-group">
-                                <input class="form-control" type="text" name="max[]" value='<?php echo intval($location['max_thresh']); ?>' /><span class="input-group-addon">%</span>
-                            </div>
-                        </td>
-                        <td class="col-lg-1 col-md-1 col-sm-1 col-xs-1 align-right">
-                            <a href="javascript:void(0);" class="btn btn-warning store-add-del"><i class="fa fa-times fa-fw"></i></a>
-                        </td>
-                    </tr>
-                    <tr class="table-storage-tr-info">
-                        <td colspan="5">Storage Estimation: <?php echo $days; ?> days <?php echo $hours; ?> hours and <?php echo $minutes; ?> minutes</td>
-                    </tr>
-                    <tr class="table-storage-tr-info">
-                        <td colspan="5">Storage Used: <?php echo $used_storage . "%"; ?></td>
-                    </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</form>
+    public function getData()
+    {
+        $this->initProc();
+    }
 
-<!-- Hidden table row template for adding new storage locations -->
-<table id="table-storage-orig" style="display: none;">
-    <tr class="table-storage-tr-data">
-        <td class="col-lg-1">
-            <label class="control-label"><?php echo LOCATION; ?></label>
-        </td>
-        <td class="col-lg-6">
-            <input class="form-control storage-path" name="path[]" type="text" value="" />
-        </td>
-        <td class="col-lg-1 align-right">
-            <label class="control-label">Max:</label>
-        </td>
-        <td class="col-lg-2">
-            <div class="input-group">
-                <input class="form-control" type="text" name="max[]" value="95" /><span class="input-group-addon">%</span>
-            </div>
-        </td>
-        <td class="col-lg-2 align-right">
-            <a href="" class="btn btn-warning store-add-del"><?php echo L_DELETE; ?></a>
-        </td>
-    </tr>
-</table>
+    public function postData()
+    {
+        $this->initProc();
+    }
+
+    private function initProc()
+    {
+        $change_status = $this->change_directory_permissions($_GET['path']);
+        data::responseJSON($change_status[0], $change_status[1]);
+    }
+
+    // Function to call the bash script to change permissions of a directory
+
+public function change_directory_permissions($path)
+{
+    // Call the bash script to change directory permissions and ownership
+    // This will also handle directory creation if it doesn't exist
+    $output = shell_exec("sudo /usr/share/bluecherry/scripts/check_dir_permission.sh " . escapeshellarg($path));
+
+    // Interpret the output from the script to form the response
+    if (strpos($output, 'Error') !== false) {
+        return array('F', $output);
+    } elseif (strpos($output, 'Changing permissions') !== false || strpos($output, 'Modifying permissions and ownership') !== false) {
+        return array('OK', 'Permissions and ownership changed for ' . $path);
+    } elseif (strpos($output, 'already has the correct permissions and ownership') !== false) {
+        return array('OK', '');
+    } elseif (strpos($output, 'Directory does not exist. Creating it now') !== false) {
+        return array('OK', 'Directory created and permissions set for ' . $path);
+    } else {
+        return array('F', 'Unexpected output from shell script: ' . $output);
+    }
+}
+
+}
