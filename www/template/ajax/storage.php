fbce8c1c5089b3462f5df9d531cf4f1e5a2a128a
Curtis Hall <curt@bluecherry.net>
2024-01-09 09:50:58 -0600

    Correct misspelling
    Was STRAGE_HEADER

--- www/template/ajax/storage.php
+++ www/template/ajax/storage.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-lg-12">
-        <h1 class="page-header"><?php echo STRAGE_HEADER; ?></h1>
+        <h1 class="page-header"><?php echo STORAGE_HEADER; ?></h1>
     </div>
 </div>
 
