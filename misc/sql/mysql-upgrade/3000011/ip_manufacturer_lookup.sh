7b939fac409a9f9fcb686487abce8e894e9cf0c0
Andriy Utkin <hello@autkin.net>
2024-02-15 00:50:14 +0000

    mysql-upgrade: fix 3000011
    Quotes around literal strings disappeared at shell level, making MySQL
    think they are expressions.
    
    Changed the outer quoting to double quotes and dropped the backticks
    around the table name.

--- misc/sql/mysql-upgrade/3000011/ip_manufacturer_lookup.sh
+++ misc/sql/mysql-upgrade/3000011/ip_manufacturer_lookup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Enable live lookups of IP camera manufacturers (pulled from our API)
 
-echo '
-INSERT INTO `GlobalSettings` VALUES ('G_DATA_SOURCE','live');
-' | mysql -h"$host" -D"$dbname" -u"$user" -p"$password"
+echo "
+INSERT INTO GlobalSettings VALUES ('G_DATA_SOURCE','live');
+" | mysql -h"$host" -D"$dbname" -u"$user" -p"$password"
 
 exit 0
