c605ce35fd043c1721fd66774a13f03ea9a1033e
Andriy Utkin <autkin@bluecherrydvr.com>
2024-03-05 11:35:52 +0000

    installer: reindent check_distro()

--- installer/v3.sh
+++ installer/v3.sh
@@ -27,15 +27,15 @@ check_distro()
     
     if [[ -e /etc/os-release ]] && [[ $ID == "centos" ]]
     then
-            DISTRO=${ID}_${VERSION_ID}
+        DISTRO=${ID}_${VERSION_ID}
     else
-    
-    if [[ -e /etc/lsb-release ]]
-    then
-        DISTRO=$DISTRIB_CODENAME
-    else
-        DISTRO=`echo $VERSION | sed -e 's/^.*[(]//' -e 's/[)]//'`
-    fi
+
+        if [[ -e /etc/lsb-release ]]
+        then
+            DISTRO=$DISTRIB_CODENAME
+        else
+            DISTRO=`echo $VERSION | sed -e 's/^.*[(]//' -e 's/[)]//'`
+        fi
     fi
     echo $DISTRO
 }
