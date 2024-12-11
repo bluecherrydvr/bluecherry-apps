bluecherrydvr/bluecherry-apps

commit 089e6de08de8028785bb16812cef16e4e07e5e4d

motion_processor: release cv::Mat objects
==6284== 1,036,944 bytes in 2 blocks are possibly lost in loss record 3,790 of 3,797
==6284==    at 0x4846828: malloc (in /usr/libexec/valgrind/vgpreload_memcheck-amd64-linux.so)
==6284==    by 0x66E9628: cv::fastMalloc(unsigned long) (in /usr/lib/x86_64-linux-gnu/libopencv_core.so.4.6.0)
==6284==    by 0x67B851B: ??? (in /usr/lib/x86_64-linux-gnu/libopencv_core.so.4.6.0)
==6284==    by 0x67AE636: cv::Mat::create(int, int const*, int) (in /usr/lib/x86_64-linux-gnu/libopencv_core.so.4.6.0)
==6284==    by 0x67AE98F: cv::Mat::create(int, int, int) (in /usr/lib/x86_64-linux-gnu/libopencv_core.so.4.6.0)
==6284==    by 0x13354C: motion_processor::detect_opencv(AVFrame*) (motion_processor.cpp:577)
==6284==    by 0x1355B2: detect (motion_processor.cpp:255)
==6284==    by 0x1355B2: motion_processor::run() (motion_processor.cpp:182)
==6284==    by 0x13577C: bc_mproc_thread(void*) (motion_processor.cpp:853)
==6284==    by 0x736EA93: start_thread (pthread_create.c:447)
==6284==    by 0x73FBA33: clone (clone.S:100)

server/motion_processor.cpp
@@ -55,6 +55,9 @@ motion_processor::~motion_processor()
 	}
 #endif
 
+	for (int i = 0; i < sizeof(m_refFrames); i++) {
+		m_refFrames[i].release();
+	}
 	delete output_source;
 }
 
@@ -576,6 +579,7 @@ int motion_processor::detect_opencv(AVFrame *rawFrame)
     // first things first... get the original frame from the AVFrame and convert it to an OpenCV Grayscale Mat, and the "downscale" size.
     cv::Mat m = cv::Mat(dst_h, dst_w, CV_8UC1);
     if (convert_AVFrame_to_grayMat(rawFrame, m) == 0) {
+	    m.release();
         return 0; // error converting frame...
     }
 
@@ -616,6 +620,7 @@ int motion_processor::detect_opencv(AVFrame *rawFrame)
 
 	if (m_motionDebug)
         check_for_new_debug_event(ret);
+	m.release();
 	return ret;
 }
 
@@ -627,8 +632,10 @@ int motion_processor::detect_opencv_advanced(AVFrame *rawFrame)
 
     // first things first... get the original frame from the AVFrame and convert it to an OpenCV Grayscale Mat, and the "downscale" size.
     cv::Mat m = cv::Mat(dst_h, dst_w, CV_8UC1);
-    if (convert_AVFrame_to_grayMat(rawFrame, m) == 0)
+    if (convert_AVFrame_to_grayMat(rawFrame, m) == 0) {
+        m.release();
         return 0; // error converting frame...
+    }
 
     // do a quick(-ish) blur to reduce noise...
     cv::GaussianBlur(m, m, cv::Size(21, 21), 0);
@@ -662,6 +669,7 @@ int motion_processor::detect_opencv_advanced(AVFrame *rawFrame)
     }
 
     m_motionTriggered = (num_md_frames >= m_minMotionFrames);
+    m.release();
     return m_motionTriggered;
 }
 

commit 77e9494be87ebafb0ffe8ff44c623896905b1c76

server/motion_processor.cpp: fix crash by freeing cv::Mat the right way
This happens when schedule changes from 'M' to 'C'.

This is how it looks in the logs:

 I(1/software): motion_blend_radio is set to 15:1
 I(1/software): motion_debug is set to 0
 I(1/software): Motion detected
 E(): Got connection, but failed to accept
 I(1/software): Switching to new recording schedule 'continuous'
 BUG: Segment violation at 0x0000000000000366
 Call trace:
 [0x000055db84eb8d5b]  @ /usr/sbin/bc-server
 [0x00007f9949977320]  @ /lib/x86_64-linux-gnu/libc.so.6
 [0x00007f994a5c77e9] _ZN2cv3Mat7releaseEv+0x00000000 @ /lib/x86_64-linux-gnu/libopencv_core.so.406
 [0x000055db84eb8ee4]  @ /usr/sbin/bc-server
 [0x000055db84ebc2d6]  @ /usr/sbin/bc-server
 [0x000055db84ebc8dd]  @ /usr/sbin/bc-server
 [0x00007f99499cea94]  @ /lib/x86_64-linux-gnu/libc.so.6
 [0x00007f9949a5ba34] __clone+0x00000000 @ /lib/x86_64-linux-gnu/libc.so.6

This is how it looks in the debugger:

 #0  0x00007f994a5c77e9 in cv::Mat::release() () from /lib/x86_64-linux-gnu/libopencv_core.so.406
 #1  0x000055db84eb8ee4 in motion_processor::~motion_processor (this=0x7f993000bbb0, __in_chrg=<optimized out>) at motion_processor.cpp:59
 #2  0x000055db84ebc2d6 in motion_processor::~motion_processor (this=0x7f993000bbb0, __in_chrg=<optimized out>) at motion_processor.cpp:42
 #3  motion_processor::run (this=0x7f993000bbb0) at motion_processor.cpp:201
 #4  0x000055db84ebc8dd in bc_mproc_thread (data=<optimized out>) at motion_processor.cpp:861
 #5  0x00007f99499cea94 in start_thread (arg=<optimized out>) at ./nptl/pthread_create.c:447
 #6  0x00007f9949a5ba34 in clone () at ../sysdeps/unix/sysv/linux/x86_64/clone.S:100

According to the documentation, cv::Mat automatically derefs and frees
data as needed on being destroyed (e.g. delete)

https://docs.opencv.org/4.x/d3/d63/classcv_1_1Mat.html#ae48d4913285518e2c21a3457017e716e

The bug was introduced by a memory leak fix.

Fixes: 089e6de08de8

server/motion_processor.cpp
@@ -37,6 +37,10 @@ motion_processor::motion_processor(bc_record *bcRecord)
 
 	memset(&m_debugEventTime, 0, sizeof(struct tm));
 	m_debugEventTime.tm_sec = -1;
+
+	// m_refFrames[2] is a static array so ctors, dtors are not called automatically on elements
+	new(&m_refFrames[0]) cv::Mat();
+	new(&m_refFrames[1]) cv::Mat();
 }
 
 motion_processor::~motion_processor()
@@ -55,9 +59,9 @@ motion_processor::~motion_processor()
 	}
 #endif
 
-	for (int i = 0; i < sizeof(m_refFrames); i++) {
-		m_refFrames[i].release();
-	}
+	// m_refFrames[2] is a static array so ctors, dtors are not called automatically on elements
+	m_refFrames[0].~Mat();
+	m_refFrames[1].~Mat();
 	delete output_source;
 }
 
@@ -579,7 +583,6 @@ int motion_processor::detect_opencv(AVFrame *rawFrame)
     // first things first... get the original frame from the AVFrame and convert it to an OpenCV Grayscale Mat, and the "downscale" size.
     cv::Mat m = cv::Mat(dst_h, dst_w, CV_8UC1);
     if (convert_AVFrame_to_grayMat(rawFrame, m) == 0) {
-	    m.release();
         return 0; // error converting frame...
     }
 
@@ -620,7 +623,6 @@ int motion_processor::detect_opencv(AVFrame *rawFrame)
 
 	if (m_motionDebug)
         check_for_new_debug_event(ret);
-	m.release();
 	return ret;
 }
 
@@ -633,7 +635,6 @@ int motion_processor::detect_opencv_advanced(AVFrame *rawFrame)
     // first things first... get the original frame from the AVFrame and convert it to an OpenCV Grayscale Mat, and the "downscale" size.
     cv::Mat m = cv::Mat(dst_h, dst_w, CV_8UC1);
     if (convert_AVFrame_to_grayMat(rawFrame, m) == 0) {
-        m.release();
         return 0; // error converting frame...
     }
 
@@ -669,7 +670,6 @@ int motion_processor::detect_opencv_advanced(AVFrame *rawFrame)
     }
 
     m_motionTriggered = (num_md_frames >= m_minMotionFrames);
-    m.release();
     return m_motionTriggered;
 }
 